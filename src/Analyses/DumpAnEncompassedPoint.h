f7beddc17dfcb30bf3fa6c08363fa0efd41918ff
Haley Clark <gmail.com@hdeanclark>
2016-03-09 11:24:25 -0800

    Removed excess whitespace using `clang-format`.

--- src/Analyses/DumpAnEncompassedPoint.h
+++ src/Analyses/DumpAnEncompassedPoint.h
@@ -1,4 +1,4 @@
-//DumpAnEncompassedPoint.h.
+// DumpAnEncompassedPoint.h.
 
 #pragma once
 
@@ -6,9 +6,9 @@
 #include <sstream>
 #include <fstream>
 #include <iomanip>
-#include <string>    
+#include <string>
 #include <vector>
-#include <set> 
+#include <set>
 #include <map>
 #include <unordered_map>
 #include <list>
@@ -19,31 +19,31 @@
 #include <limits>
 #include <cmath>
 
-#include <getopt.h>           //Needed for 'getopts' argument parsing.
-#include <cstdlib>            //Needed for exit() calls.
-#include <utility>            //Needed for std::pair.
+#include <getopt.h> //Needed for 'getopts' argument parsing.
+#include <cstdlib>  //Needed for exit() calls.
+#include <utility>  //Needed for std::pair.
 #include <algorithm>
 #include <experimental/optional>
 
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
 
-#include "YgorMisc.h"         //Needed for FUNCINFO, FUNCWARN, FUNCERR macros.
-#include "YgorMath.h"         //Needed for vec3 class.
+#include "YgorMisc.h"                //Needed for FUNCINFO, FUNCWARN, FUNCERR macros.
+#include "YgorMath.h"                //Needed for vec3 class.
 #include "YgorMathPlottingGnuplot.h" //Needed for YgorMathPlottingGnuplot::*.
-#include "YgorMathChebyshev.h" //Needed for cheby_approx class.
-#include "YgorStats.h"        //Needed for Stats:: namespace.
-#include "YgorFilesDirs.h"    //Needed for Does_File_Exist_And_Can_Be_Read(...), etc..
-#include "YgorContainers.h"   //Needed for bimap class.
-#include "YgorPerformance.h"  //Needed for YgorPerformance_dt_from_last().
-#include "YgorAlgorithms.h"   //Needed for For_Each_In_Parallel<..>(...)
-#include "YgorArguments.h"    //Needed for ArgumentHandler class.
-#include "YgorString.h"       //Needed for GetFirstRegex(...)
+#include "YgorMathChebyshev.h"       //Needed for cheby_approx class.
+#include "YgorStats.h"               //Needed for Stats:: namespace.
+#include "YgorFilesDirs.h"           //Needed for Does_File_Exist_And_Can_Be_Read(...), etc..
+#include "YgorContainers.h"          //Needed for bimap class.
+#include "YgorPerformance.h"         //Needed for YgorPerformance_dt_from_last().
+#include "YgorAlgorithms.h"          //Needed for For_Each_In_Parallel<..>(...)
+#include "YgorArguments.h"           //Needed for ArgumentHandler class.
+#include "YgorString.h"              //Needed for GetFirstRegex(...)
 #include "YgorImages.h"
 #include "YgorImagesIO.h"
 #include "YgorImagesPlotting.h"
 
-#include "Explicator.h"       //Needed for Explicator class.
+#include "Explicator.h" //Needed for Explicator class.
 
 #include "../Structs.h"
 
@@ -89,5 +89,6 @@
 
 
 
-
-Drover DumpAnEncompassedPoint(Drover DICOM_data, std::map<std::string,std::string> /*InvocationMetadata*/, std::string /*FilenameLex*/);
+Drover DumpAnEncompassedPoint(Drover DICOM_data,
+                              std::map<std::string, std::string> /*InvocationMetadata*/,
+                              std::string /*FilenameLex*/);
