7c8665e13ee2939b97ae1685e0ffdacb3ab701a5
Haley Clark <gmail.com@hdeanclark>
2017-03-27 20:41:57 -0700

    Added Debian packaging scripts (via CPack).

---
+++ clean_compile_and_package_debian.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+
+BUILDDIR="/home/hal/Builds/DICOMautomaton/"
+BUILTPKGSDIR="/home/hal/Builds/"
+
+mkdir -p "${BUILDDIR}"
+rsync -avz --cvs-exclude --delete ./ "${BUILDDIR}"  # Removes CMake cache files, forcing a fresh rebuild.
+#rsync -avz --cvs-exclude ./ "${BUILDDIR}"
+
+pushd .
+cd "${BUILDDIR}"
+mkdir -p build && cd build/
+cmake ../src/ -DCMAKE_INSTALL_PREFIX=/usr
+make -j 4 && make package
+mv *.deb "${BUILTPKGSDIR}/"
+popd
+
