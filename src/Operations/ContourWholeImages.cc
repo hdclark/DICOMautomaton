0871e82f54bb77e724d3f2eb0613a99e57eaca07
Haley Clark <gmail.com@hdeanclark>
2017-11-16 11:29:04 -0800

    Changed implementation to base on new upstream routine.

--- src/Operations/ContourWholeImages.cc
+++ src/Operations/ContourWholeImages.cc
@@ -42,7 +42,6 @@
 #include "Explicator.h"       //Needed for Explicator class.
 
 #include "../Structs.h"
-#include "../Thread_Pool.h"
 
 #include "../YgorImages_Functors/Grouping/Misc_Functors.h"
 
@@ -141,6 +140,9 @@ Drover ContourWholeImages(Drover DICOM_data, OperationArgPkg OptArgs, std::map<s
         throw std::invalid_argument("Image selection is not valid. Cannot continue.");
     }
 
+    Explicator X(FilenameLex);
+    const long int ROINumber = 10001; // TODO: find highest existing and ++ it.
+
     //Construct a destination for the ROI contours.
     if(DICOM_data.contour_data == nullptr){
         std::unique_ptr<Contour_Data> output (new Contour_Data());
@@ -149,7 +151,7 @@ Drover ContourWholeImages(Drover DICOM_data, OperationArgPkg OptArgs, std::map<s
     DICOM_data.contour_data->ccs.emplace_back();
 
     DICOM_data.contour_data->ccs.back().Raw_ROI_name = ROILabel;
-    DICOM_data.contour_data->ccs.back().ROI_number = 10001; // TODO: find highest existing and ++ it.
+    DICOM_data.contour_data->ccs.back().ROI_number = ROINumber;
     DICOM_data.contour_data->ccs.back().Minimum_Separation = 1.0; // TODO: is there a routine to do this? (YES: Unique_Contour_Planes()...)
 
     //Iterate over each requested image_array. Each image is processed independently, so a thread pool is used.
@@ -160,58 +162,31 @@ Drover ContourWholeImages(Drover DICOM_data, OperationArgPkg OptArgs, std::map<s
         if(!DICOM_data.image_data.empty()) iap_it = std::prev(DICOM_data.image_data.end());
     }
     for( ; iap_it != DICOM_data.image_data.end(); ++iap_it){
-        asio_thread_pool tp;
-        std::mutex saver_printer; // Who gets to save generated contours, print to the console, and iterate the counter.
-        long int completed = 0;
-        const long int img_count = (*iap_it)->imagecoll.images.size();
-
-        for(const auto &animg : (*iap_it)->imagecoll.images){
-            if( (animg.rows < 1) || (animg.columns < 1) ){
-                throw std::runtime_error("Image is empty -- cannot contour whole image.");
-            }
-
-            tp.submit_task([&](void) -> void {
-                const auto R = animg.rows;
-                const auto C = animg.columns;
-
-                //Pin vertices to each corner of the image.
-                const auto corner_A = animg.position(0,0)     - animg.row_unit*animg.pxl_dx*0.45 - animg.col_unit*animg.pxl_dy*0.45;
-                const auto corner_B = animg.position(R-1,0)   + animg.row_unit*animg.pxl_dx*0.45 - animg.col_unit*animg.pxl_dy*0.45;
-                const auto corner_C = animg.position(R-1,C-1) + animg.row_unit*animg.pxl_dx*0.45 + animg.col_unit*animg.pxl_dy*0.45;
-                const auto corner_D = animg.position(0,C-1)   - animg.row_unit*animg.pxl_dx*0.45 + animg.col_unit*animg.pxl_dy*0.45;
-                
-                //Walk all available half-edges forming contour perimeters.
-                std::list<contour_of_points<double>> cop;
-
-                cop.emplace_back();
-                cop.back().closed = true;
-                cop.back().metadata["ROIName"] = ROILabel;
-                cop.back().metadata["Description"] = "Whole-Image Contour";
-                for(const auto &key : { "StudyInstanceUID", "FrameofReferenceUID" }){
-                    if(animg.metadata.count(key) != 0) cop.back().metadata[key] = animg.metadata.at(key);
-                }
-
-                cop.back().points.emplace_back(corner_A);
-                cop.back().points.emplace_back(corner_B);
-                cop.back().points.emplace_back(corner_C);
-                cop.back().points.emplace_back(corner_D);
-
-
-                //Save the contours and print some information to screen.
-                {
-                    std::lock_guard<std::mutex> lock(saver_printer);
-
-                    //Check if there is already a suitable contour. If so, do not bother saving the duplicate.
-                    if(! animg.encompasses_any_part_of_contour_in_collection( DICOM_data.contour_data->ccs.back() )){
-                        DICOM_data.contour_data->ccs.back().contours.splice( DICOM_data.contour_data->ccs.back().contours.end(), cop);
-                    }    
-
-                    ++completed;
-                    FUNCINFO("Completed " << completed << " of " << img_count
-                          << " --> " << static_cast<int>(1000.0*(completed)/img_count)/10.0 << "\% done");
-                }
-            }); // thread pool task closure.
+        std::list<std::reference_wrapper<planar_image<float,double>>> imgs;
+        for(auto &animg : (*iap_it)->imagecoll.images){
+            imgs.emplace_back( std::ref(animg) );
         }
+
+        Encircle_Images_with_Contours_Opts opts;
+        opts.inclusivity = Encircle_Images_with_Contours_Opts::Inclusivity::Centre;
+        opts.contouroverlap = Encircle_Images_with_Contours_Opts::ContourOverlap::Disallow;
+
+        //Prepare contour metadata using image metadata.
+        //
+        //Note: We currently attach *all* common image data to each contour. Should we filter some (most) out?
+        std::map<std::string,std::string> metadata;
+        metadata = DICOM_data.image_data.back()->imagecoll.get_common_metadata({});
+        metadata["ROINumber"] = std::to_string(ROINumber);
+        metadata["ROIName"] = ROILabel;
+        metadata["NormalizedROIName"] = X(ROILabel);
+        metadata["MinimumSeparation"] = metadata["SliceThickness"];
+        metadata["Description"] = "Whole-Image Contour";
+
+        auto cc = Encircle_Images_with_Contours(imgs, opts, metadata);
+
+        DICOM_data.contour_data->ccs.back().contours.splice(
+             DICOM_data.contour_data->ccs.back().contours.end(),
+             cc.contours);
     }
 
     return std::move(DICOM_data);
