21d224cbca5ab93e0d2116ceb51ded3ee2e98738
Hal <haley.clark@bccancer.bc.ca>
2018-04-09 16:23:59 -0700

    Make build datetime Docker tag more explicit.

--- docker/build.sh
+++ docker/build.sh
@@ -8,9 +8,11 @@ if [ ! -z "${sstat}" ] ; then
     clean_dirty="dirty"
 fi
 
+build_datetime=$(date '+%Y%m%_0d-%_0H%_0M%_0S')
+
 time sudo docker build \
     --no-cache=true \
-    -t dicomautomaton_webserver:$(date '+%Y%m%_0d-%_0H%_0M%_0S') \
+    -t dicomautomaton_webserver:"built_${build_datetime}" \
     -t dicomautomaton_webserver:"commit_${commit_id}_${clean_dirty}" \
     -t dicomautomaton_webserver:latest \
     .
