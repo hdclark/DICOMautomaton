467adb0efa7e2d4b4edee4a49c8050047f3b776d
Hal <haley.clark@bccancer.bc.ca>
2018-04-09 12:04:30 -0700

    Added git commit tags to Docker recipe file.

--- docker/build.sh
+++ docker/build.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
-time sudo docker build -t dicomautomaton_webserver .
+commit_id=$(git rev-parse HEAD)
+
+clean_dirty="clean"
+sstat=$(git diff --shortstat)
+if [ -z "${sstat}" ] ; then
+    clean_dirty="dirty"
+fi
+
+time sudo docker build \
+    --no-cache=true \
+    -t dicomautomaton_webserver:$(date '+%Y%m%_0d-%_0H%_0M%_0S') \
+    -t dicomautomaton_webserver:"commit_${commit_id}_${clean_dirty}" \
+    -t dicomautomaton_webserver:latest \
+    .
 
