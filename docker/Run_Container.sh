c9efae556a574942c21c7feab0aaf54479d77d01
Hal <haley.clark@bccancer.bc.ca>
2018-04-10 12:07:48 -0700

    Avoid hardcoded usernames in run script.

--- docker/Run_Container.sh
+++ docker/Run_Container.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-mkdir -p /home/hal/client_uploads/
+set -e 
+
+uploads_dir="${HOME}/DCMA_WS_client_uploads/"
+artifacts_dir="${HOME}/DCMA_WS_artifacts/"
+
+mkdir -p "${uploads_dir}"
+mkdir -p "${artifacts_dir}"
+
+printf "Client uploads go to: ${uploads_dir}\n"
+printf "WebServer artifacts go to: ${artifacts_dir}\n"
 
 sudo docker run -it --rm -p 8080:80 \
-    -v /home/hal/client_uploads/:/client_uploads:rw \
+    -v "${uploads_dir}":/client_uploads/:rw \
+    -v "${artifacts_dir}":/home/hal/DICOMautomaton_Webserver_Artifacts/:rw \
     dicomautomaton_webserver:latest
 
 #    --entrypoint /bin/bash \
