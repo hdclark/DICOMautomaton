a8d1863c366acb130b7a981626005cfaba0616a5
Haley Clark <gmail.com@hdeanclark>
2019-02-04 00:49:18 -0800

    Added basic man page render of reference guide.

--- documentation/compile_documentation.sh
+++ documentation/compile_documentation.sh
@@ -14,6 +14,7 @@ for i in ./*.md ; do
     filename_in="$(basename "${file_in}")"
     pdf_file_out="./${filename_in%.*}.pdf"
     htm_file_out="./${filename_in%.*}.html"
+    man_file_out="./${filename_in%.*}.man"
 
     # Build the compiler invocations.
     pdf_compiler_inv=()
@@ -40,6 +41,15 @@ for i in ./*.md ; do
     htm_compiler_inv+=(--number-sections)
     htm_compiler_inv+=(--output="${htm_file_out}")
 
+    man_compiler_inv=()
+    man_compiler_inv+=(pandoc)
+    man_compiler_inv+=("${file_in}")
+    man_compiler_inv+=(--from=markdown+raw_tex)
+    man_compiler_inv+=(--to=man)
+    #man_compiler_inv+=(--standalone)
+    man_compiler_inv+=(--self-contained)
+    man_compiler_inv+=(--output="${man_file_out}")
+
     if false ; then
         exit
     elif command -v pandoc >/dev/null 2>&1 ; then
@@ -47,14 +57,17 @@ for i in ./*.md ; do
         printf "Compiling '%s' with system pandoc.\n" "$i"
         "${pdf_compiler_inv[@]}"
         "${htm_compiler_inv[@]}"
+        "${man_compiler_inv[@]}"
     elif [ -f ~/Appliances/docutools/Run_Container.sh ] ; then
         # If the docutools docker container is available, use it.
         printf "Compiling '%s' with docutools Docker container pandoc.\n" "$i"
         ~/Appliances/docutools/Run_Container.sh "${pdf_compiler_inv[@]}"
         ~/Appliances/docutools/Run_Container.sh "${htm_compiler_inv[@]}"
+        ~/Appliances/docutools/Run_Container.sh "${man_compiler_inv[@]}"
 
         sudo chown $(id -un):$(id -gn) "${pdf_file_out}" 
         sudo chown $(id -un):$(id -gn) "${htm_file_out}" 
+        sudo chown $(id -un):$(id -gn) "${man_file_out}" 
     else
         printf "Cannot find a way to run pandoc. Cannot continue.\n"
         exit 1
