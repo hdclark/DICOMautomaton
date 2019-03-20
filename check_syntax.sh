328f7184a50dd092e771c8aa82afcfc155d3ccf4
Haley Clark <gmail.com@hdeanclark>
2019-03-20 11:18:47 -0700

    Support other locales explicitly.
    I noticed there was a problem reading some Boost headers on Debian if a utf8 locale is not available.
    This will help diagnose the issue and could be useful to test portability.

--- check_syntax.sh
+++ check_syntax.sh
@@ -2,18 +2,39 @@
 
 # This script can be used to verify the syntax of all files without actually compiling anything.
 
+#locale="en_US.UTF-8"
+locale="C"
+export LANG="$locale"
+export LANGUAGE=""
+export LC_CTYPE="$locale"
+export LC_NUMERIC="$locale"
+export LC_TIME="$locale"
+export LC_COLLATE="$locale"
+export LC_MONETARY="$locale"
+export LC_MESSAGES="$locale"
+export LC_PAPER="$locale"
+export LC_NAME="$locale"
+export LC_ADDRESS="$locale"
+export LC_TELEPHONE="$locale"
+export LC_MEASUREMENT="$locale"
+export LC_IDENTIFICATION="$locale"
+export LC_ALL=""
+
 
 # Check all files in the project.
-#find ./src/ -type f -execdir \
-#    g++ --std=c++14 -fsyntax-only '{}' \+
+#find ./src/ -type f -print0 |
+#    grep -z -E '*[.]cc|*[.]cpp' |
+#    grep -z -i -v '.*imebra.*' |
+#    xargs -0 -I '{}' -P $(nproc || echo 2) -n 1 -r \
+#      g++ --std=c++17 -fsyntax-only '{}'
 
+# Compile, but do not link:
+#      g++ --std=c++17 -c '{}' -o /dev/null 
 
 # Check only modified and untracked files.
 git ls-files -z -o -m |
   grep -z -E '*[.]h|*[.]cc|*[.]cpp' |
+    grep -z -i -v '.*imebra.*' |
   xargs -0 -I '{}' -P $(nproc || echo 2) -n 1 -r \
-    g++ --std=c++14 -fsyntax-only '{}'
-
-# Compile, but do not link:
-#   g++ --std=c++14 -c in.cc -o /dev/null 
+    g++ --std=c++17 -fsyntax-only '{}'
 
