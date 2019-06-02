a9ba88cb2e713e248fcb7cf5b72af1f05b573058
Haley Clark <gmail.com@hdeanclark>
2019-06-02 13:58:54 -0700

    SubtractImages: support more advanced image selection.
    This operation was originally written long ago, well prior to the existing and widely-used regex-based image selection
    capabilities. Multiple images can now be selected, and both images can be specified rather than assuming the last image
    was selected (i.e., moving away from the earlier stack-based image processing workflow).

--- src/Operations/SubtractImages.cc
+++ src/Operations/SubtractImages.cc
@@ -21,74 +21,57 @@ OperationDoc OpArgDocSubtractImages(void){
     OperationDoc out;
     out.name = "SubtractImages";
     out.desc = 
-        " This routine subtracts images that spatially overlap.";
+        "This routine subtracts images that spatially overlap.";
 
     out.notes.emplace_back(
-        "This operation currently performs a subtraction necessarily using the first image volume."
+        "The ReferenceImageSelection is subtracted from the ImageSelection and the result is stored"
+        " in ImageSelection. So this operation implements $A = A - B$ where A is ImageSelection and"
+        " B is ReferenceImageSelection. The ReferenceImageSelection images are not altered."
     );
     out.notes.emplace_back(
-        "This operation is currently extremely limited in that the selected images must be selected by position."
-        " A more flexible approach will be eventually be implemented when the image selection mechanism is overhauled."
+        "Multiple image volumes can be selected by both ImageSelection and ReferenceImageSelection."
+        " For each ImageSelection volume, each of the ReferenceImageSelection volumes are subtracted"
+        " sequentially."
     );
 
     out.args.emplace_back();
+    out.args.back() = IAWhitelistOpArgDoc();
     out.args.back().name = "ImageSelection";
-    out.args.back().desc = "Images to operate on. Either 'none', 'last', 'first', or 'all'.";
-    out.args.back().default_val = "all";
-    out.args.back().expected = true;
-    out.args.back().examples = { "none", "last", "first", "all" };
+    out.args.back().default_val = "last";
+
+    out.args.emplace_back();
+    out.args.back() = IAWhitelistOpArgDoc();
+    out.args.back().name = "ReferenceImageSelection";
+    out.args.back().default_val = "!last";
 
     return out;
 }
-
-
 
 Drover SubtractImages(Drover DICOM_data, OperationArgPkg OptArgs, std::map<std::string,std::string>, std::string ){
 
     //---------------------------------------------- User Parameters --------------------------------------------------
     const auto ImageSelectionStr = OptArgs.getValueStr("ImageSelection").value();
+    const auto ReferenceImageSelectionStr = OptArgs.getValueStr("ReferenceImageSelection").value();
 
     //-----------------------------------------------------------------------------------------------------------------
-    const auto regex_none  = Compile_Regex("^no?n?e?$");
-    const auto regex_first = Compile_Regex("^fi?r?s?t?$");
-    const auto regex_last  = Compile_Regex("^la?s?t?$");
-    const auto regex_all   = Compile_Regex("^al?l?$");
 
-    if( !std::regex_match(ImageSelectionStr, regex_none)
-    &&  !std::regex_match(ImageSelectionStr, regex_first)
-    &&  !std::regex_match(ImageSelectionStr, regex_last)
-    &&  !std::regex_match(ImageSelectionStr, regex_all) ){
-        throw std::invalid_argument("Image selection is not valid. Cannot continue.");
-    }
+    auto RIAs_all = All_IAs( DICOM_data );
+    auto RIAs = Whitelist( RIAs_all, ReferenceImageSelectionStr );
 
-    auto iapA_it = DICOM_data.image_data.begin();
-    std::list<std::shared_ptr<Image_Array>> diffs;
+    auto IAs_all = All_IAs( DICOM_data );
+    auto IAs = Whitelist( IAs_all, ImageSelectionStr );
+    for(auto & iap_it : IAs){
 
-    auto iapB_it = DICOM_data.image_data.begin();
-    if(false){
-    }else if(std::regex_match(ImageSelectionStr, regex_none)){ iapB_it = DICOM_data.image_data.end();
-    }else if(std::regex_match(ImageSelectionStr, regex_last)){
-        if(!DICOM_data.image_data.empty()) iapB_it = std::prev(DICOM_data.image_data.end());
-    }
-    while(iapB_it != DICOM_data.image_data.end()){
+        for(auto & riap_it : RIAs){
 
-        std::shared_ptr<Image_Array> difference = std::make_shared<Image_Array>(*(*iapA_it));
-        {
-          std::list<std::reference_wrapper<planar_image_collection<float,double>>> external_imgs;
-          external_imgs.push_back( std::ref((*iapB_it)->imagecoll) );
+            std::list<std::reference_wrapper<planar_image_collection<float,double>>> external_imgs;
+            external_imgs.push_back( std::ref( (*riap_it)->imagecoll ) );
 
-          if(!difference->imagecoll.Transform_Images( SubtractSpatiallyOverlappingImages, std::move(external_imgs), {} )){
-              FUNCERR("Unable to subtract the pixel maps");
-          }
+            if(!(*iap_it)->imagecoll.Transform_Images( SubtractSpatiallyOverlappingImages, 
+                                                       std::move(external_imgs), {} )){
+                throw std::runtime_error("Unable to subtract images.");
+            }
         }
-        diffs.emplace_back( difference );
-
-        ++iapB_it;
-        if(std::regex_match(ImageSelectionStr, regex_first)) break;
-    }
-
-    for(auto &iap : diffs){
-        DICOM_data.image_data.emplace_back(iap);
     }
 
     return DICOM_data;
