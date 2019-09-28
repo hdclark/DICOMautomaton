95e74afc2266a83374894b39cf3fe12b6b0cb925
Hal Clark <gmail.com@hdeanclark>
2019-09-28 09:34:00 -0700

    ConvertContoursToPoints: support extracting the centroid.

--- src/Operations/ConvertContoursToPoints.cc
+++ src/Operations/ConvertContoursToPoints.cc
@@ -43,13 +43,6 @@ OperationDoc OpArgDocConvertContoursToPoints(void){
     );
         
 
-    out.args.emplace_back();
-    out.args.back().name = "Label";
-    out.args.back().desc = "A label to attach to the point cloud.";
-    out.args.back().default_val = "unspecified";
-    out.args.back().expected = true;
-    out.args.back().examples = { "unspecified", "POIs", "peaks", "above_zero", "below_5.3" };
-
 
     out.args.emplace_back();
     out.args.back().name = "NormalizedROILabelRegex";
@@ -64,6 +57,7 @@ OperationDoc OpArgDocConvertContoursToPoints(void){
                             R"***(.*Left.*Parotid.*|.*Right.*Parotid.*|.*Eye.*)***",
                             R"***(Left Parotid|Right Parotid)***" };
 
+
     out.args.emplace_back();
     out.args.back().name = "ROILabelRegex";
     out.args.back().desc = "A regex matching ROI labels/names to consider. The default will match"
@@ -77,6 +71,34 @@ OperationDoc OpArgDocConvertContoursToPoints(void){
                                  R"***(.*left.*parotid.*|.*right.*parotid.*|.*eyes.*)***",
                                  R"***(left_parotid|right_parotid)***" };
 
+
+    out.args.emplace_back();
+    out.args.back().name = "Label";
+    out.args.back().desc = "A label to attach to the point cloud.";
+    out.args.back().default_val = "unspecified";
+    out.args.back().expected = true;
+    out.args.back().examples = { "unspecified", "POIs", "peaks", "above_zero", "below_5.3" };
+
+
+    out.args.emplace_back();
+    out.args.back().name = "Method";
+    out.args.back().desc = "The conversion method to use."
+                           " Two options are available: 'vertices' and 'centroid'."
+                           ""
+                           " The 'vertices' option extracts all vertices from all selected contours and directly"
+                           " inserts them into the new point cloud."
+                           " Point clouds created this way will contain as many points as there are contour vertices.";
+                           ""
+                           " The 'centroid' option finds the centroid of all vertices from all selected contours."
+                           " Note that the centroid gives every point an equal weighting, so heterogeneous contour"
+                           " vertex density will shift the position of the centroid (unless the distribution is"
+                           " symmetric about the centroid, which should roughly be the case for spherical contour"
+                           " collections)."
+                           " Point clouds created this way will contain a single point.";
+    out.args.back().default_val = "vertices";
+    out.args.back().expected = true;
+    out.args.back().examples = { "vertices", "centroid" };
+
     return out;
 }
 
@@ -87,12 +109,15 @@ Drover ConvertContoursToPoints(Drover DICOM_data, OperationArgPkg OptArgs, std::
     Explicator X(FilenameLex);
 
     //---------------------------------------------- User Parameters --------------------------------------------------
-    const auto LabelStr = OptArgs.getValueStr("Label").value();
-
     const auto NormalizedROILabelRegex = OptArgs.getValueStr("NormalizedROILabelRegex").value();
     const auto ROILabelRegex = OptArgs.getValueStr("ROILabelRegex").value();
 
+    const auto LabelStr = OptArgs.getValueStr("Label").value();
+
+    const auto MethodStr = OptArgs.getValueStr("Method").value();
     //-----------------------------------------------------------------------------------------------------------------
+    const auto regex_vertices = Compile_Regex("^ve?r?t?i?c?e?s?$");
+    const auto regex_centroid = Compile_Regex("^ce?n?t?r?o?i?d?$");
 
     //Stuff references to all contours into a list. Remember that you can still address specific contours through
     // the original holding containers (which are not modified here).
@@ -106,15 +131,37 @@ Drover ConvertContoursToPoints(Drover DICOM_data, OperationArgPkg OptArgs, std::
     //Construct a destination for the point clouds.
     DICOM_data.point_data.emplace_back( std::make_unique<Point_Cloud>() );
 
-    for(auto & cc_refw : cc_ROIs){
-        for(const auto & c : cc_refw.get().contours){
-            for(const auto & v : c.points){
-                DICOM_data.point_data.back()->pset.points.emplace_back( v );
+    if(false){
+    }else if( std::regex_match(MethodStr, regex_vertices) ){
+        // Insert all vertices into the point cloud.
+        for(auto & cc_refw : cc_ROIs){
+            for(const auto & c : cc_refw.get().contours){
+                for(const auto & v : c.points){
+                    DICOM_data.point_data.back()->pset.points.emplace_back( v );
+                }
             }
         }
-    }
 
-////////////// TODO ///////////////
+    }else if( std::regex_match(MethodStr, regex_centroid) ){
+        // Insert all vertices into the point cloud.
+        for(auto & cc_refw : cc_ROIs){
+            for(const auto & c : cc_refw.get().contours){
+                for(const auto & v : c.points){
+                    DICOM_data.point_data.back()->pset.points.emplace_back( v );
+                }
+            }
+        }
+        
+        // Determine the centroid.
+        const auto centroid = DICOM_data.point_data.back()->pset.Centroid();
+
+        // Replace the point cloud points with only the centroid.
+        DICOM_data.point_data.back()->pset.points.clear();
+        DICOM_data.point_data.back()->pset.points.emplace_back( centroid );
+
+    }else{
+        throw std::invalid_argument("Method not understood. Cannot continue.");
+    }
 
     // Determine the common set of contour metadata and assign it to the point data.
     {
