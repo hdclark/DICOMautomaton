155a805cf9fd54c47d16279d5b020d6ca0943902
Haley Clark <gmail.com@hdeanclark>
2020-04-20 23:44:39 -0700

    Linux: update host-to-linux-image transplant script.
    The DICOMautomaton binaries are now all accessible inside the container, along with necessary shared libraries and
    especially libc. Some run-time components will be missing, e.g., explicator-provided lexicons, locale information, core
    binutils binaries, and lots of other functionality. However, this may be sufficient for long-time archival of an
    immutable, self-contained, minimal system capable of providing DICOMautomaton tooling.
    
    Further testing is required to ensure all runtime functionality (e.g., graphical support) is available.

--- linux/Run.sh
+++ linux/Run.sh
@@ -18,80 +18,159 @@ cd linux*/
 #make help
 make clean || true
 make x86_64_defconfig
+#make menuconfig
 make -j $(nproc)
 cp arch/x86/boot/bzImage ../
 cd ../
 
-##############################################
-# Busybox.
-##############################################
-if [ ! -f busybox.tar.bz2 ] ; then
-    wget 'https://busybox.net/downloads/busybox-1.31.1.tar.bz2' -O busybox.tar.bz2
-    tar axf busybox.tar.bz2
-fi
-cd busybox*/
-
-#make help
-make clean || true
-make defconfig
-#make menuconfig
-
-cp .config .config_defconfig
-sed -i \
-  -e 's@.*CONFIG_STATIC.*@CONFIG_STATIC=y@g' \
-  -e 's@.*CONFIG_BASH_IS_NONE.*@# CONFIG_BASH_IS_NONE is not set@g' \
-  -e 's@.*CONFIG_BASH_IS_HUSH.*@CONFIG_BASH_IS_HUSH=y@g' \
-  -e 's@.*CONFIG_PREFIX.*@CONFIG_PREFIX="./_install"@g' \
-  -e 's@.*CONFIG_DESKTOP=.*@CONFIG_DESKTOP=n@g' \
-  -e 's@.*CONFIG_EXTRA_CFLAGS.*@CONFIG_EXTRA_CFLAGS="-m64"@g' \
-  -e 's@.*CONFIG_EXTRA_LDFLAGS.*@CONFIG_EXTRA_LDFLAGS="-m64"@g' \
-  .config
-
-make -j $(nproc) CC=musl-gcc install # Note: installs to a local directory. See config above.
-#cp busybox ../
-cd ../
+###############################################
+## Busybox.
+###############################################
+#if [ ! -f busybox.tar.bz2 ] ; then
+#    wget 'https://busybox.net/downloads/busybox-1.31.1.tar.bz2' -O busybox.tar.bz2
+#    tar axf busybox.tar.bz2
+#fi
+#cd busybox*/
+#
+##make help
+#make clean || true
+#make defconfig
+##make menuconfig
+#
+#cp .config .config_defconfig
+#sed -i \
+#  -e 's@.*CONFIG_STATIC.*@CONFIG_STATIC=y@g' \
+#  -e 's@.*CONFIG_BASH_IS_NONE.*@# CONFIG_BASH_IS_NONE is not set@g' \
+#  -e 's@.*CONFIG_BASH_IS_HUSH.*@CONFIG_BASH_IS_HUSH=y@g' \
+#  -e 's@.*CONFIG_PREFIX.*@CONFIG_PREFIX="./_install"@g' \
+#  -e 's@.*CONFIG_DESKTOP=.*@CONFIG_DESKTOP=n@g' \
+#  -e 's@.*CONFIG_EXTRA_CFLAGS.*@CONFIG_EXTRA_CFLAGS="-m64"@g' \
+#  -e 's@.*CONFIG_EXTRA_LDFLAGS.*@CONFIG_EXTRA_LDFLAGS="-m64"@g' \
+#  .config
+#
+#make -j $(nproc) CC=musl-gcc install # Note: installs to a local directory. See config above.
+##make -j $(nproc) install # Note: installs to a local directory. See config above.
+##cp busybox ../
+#cd ../
 
 ##############################################
 # Root filesystem.
 ##############################################
 rm -rf rootfs || true
 mkdir rootfs
-rsync -a ./busybox*/_install/ ./rootfs/
-cp ./rootfs/bin/hush ./rootfs/bin/bash
-#chmod 777 ./rootfs/bin/busybox
-mkdir rootfs/dev
+mkdir rootfs/{bin,sbin,lib,dev,run,etc,mnt,proc,sbin,sys,tmp,usr}
+#rsync -a ./busybox*/_install/ ./rootfs/
 (
-cd rootfs/dev
-sudo mknod ram0 b 1 0
-sudo mknod ram1 b 1 1
-sudo mknod random c 1 8
-sudo mknod urandom c 1 9
-sudo mknod tty0 c 4 0
-sudo mknod ttyS0 c 4 64
-sudo mknod ttyS1 c 4 65
-sudo mknod tty c 5 0
-sudo mknod null c 1 3
-sudo mknod zero c 1 5
-sudo mknod kmsg c 1 11
-sudo mknod console c 5 1
+  cd rootfs/dev
+  sudo mknod ram0 b 1 0
+  sudo mknod ram1 b 1 1
+  sudo mknod random c 1 8
+  sudo mknod urandom c 1 9
+  sudo mknod tty0 c 4 0
+  sudo mknod ttyS0 c 4 64
+  sudo mknod ttyS1 c 4 65
+  sudo mknod tty c 5 0
+  sudo mknod -m 664 null c 1 3
+  sudo mknod zero c 1 5
+  sudo mknod kmsg c 1 11
+  sudo mknod -m 640 console c 5 1
+  sudo mkdir -pv usr/lib
+)
+(
+  cd rootfs/
+  ln -s lib lib64
+  ln -s ../bin usr/bin
+  #mkdir -p usr/lib
+  ln -s ../lib usr/lib
+  ln -s proc/self/fd dev/fd
+  ln -s proc/self/fd/0 stdin
+  ln -s proc/self/fd/1 stdout
+  ln -s proc/self/fd/2 stderr
+  ln -s proc/kcore kcore
 )
 
-rsync -a /tmp/portable_dcma/ ./rootfs/portable_dcma/
-
 (
-cat <<'EOF'
-#!/bin/sh
-echo "Running simple init now..."
-mkdir /proc /sys /tmp
-mount -t proc  none /proc
-mount -t sysfs none /sys
-mount -t tmpfs none /tmp
-/usr/bin/setsid /bin/cttyhack /bin/sh
-exec /bin/sh #sbin/init
-EOF
+  echo '#!/bin/bash'
+  echo 'mkdir -p /proc /sys /tmp'
+  echo 'mount -t proc  none /proc'
+  echo 'mount -t sysfs none /sys'
+  echo 'mount -t tmpfs none /tmp'
+# Busybox fix:
+#  echo '/sbin/mdev -s'
+#  echo '/usr/bin/setsid /bin/cttyhack /bin/sh'
+  echo 'exec /bin/bash'
 ) > rootfs/init
 chmod 777 rootfs/init
 
+# Copy the host's glibc.
+#(
+#  cd rootfs/
+#  pacman -Ql bash glibc binutils |
+#    sed -e 's/^glibc //g' |
+#    sed -e 's/^bash //g' |
+#    sort |
+#    while read afile ; do
+#        if [ -d "$afile" ] ; then
+#            sudo mkdir -pv ."$afile"
+#        else
+#            sudo cp -av "$afile" ."$afile"
+#            #rsync -av -n /"$afile" ./rootfs/"$afile"
+#        fi
+#    done
+#)
+
+# Copy binaries and all needed libraries.
+# Note that non-library runtime components may be missing!
+unsorted=$(mktemp /tmp/unsorted.XXXXXXXXXXXXX)
+for f in \
+  sh cat cp ls mkdir mknod mktemp mount sed \
+  sleep ln rm uname readlink basename bash \
+  dirname vi printenv free htop du df \
+  dicomautomaton_dispatcher \
+  dicomautomaton_webserver \
+  dicomautomaton_dump \
+  zenity \
+  dialog ; do
+    d="/bin"
+    if   [ -e "/bin/$f" ] ; then   d="/bin"
+    elif [ -e "/sbin/$f" ] ; then  d="/sbin"
+    else
+        printf 'Unable to locate host binary. Cannot continue.\n' 1>&2
+        exit 1
+    fi
+    ldd "$d/$f" | sed -e 's/\t//g' | cut -d' ' -f1 >> $unsorted
+    sudo cp -aL "$d/$f" ./rootfs/bin/
+done
+
+sort $unsorted |
+  uniq |
+  while read alib ; do
+      if [[ "$alib" == linux-vdso.so.1 ]] ||
+         [[ "$alib" == linux-gate.so.1 ]] ||
+         [[ "$alib" == libsystemd-shared* ]] ; then
+          continue
+      fi
+      d="/lib/"
+      if   [ -e "$alib" ] ; then            d="" # Library already contains a prefix.
+      elif [ -e "/lib/$alib" ] ; then       d="/lib/"
+      elif [ -e "/lib64/$alib" ] ; then     d="/lib64/"
+      elif [ -e "/usr/lib/$alib" ] ; then   d="/usr/lib/"
+      else
+          printf 'Unable to locate library "%s". Cannot continue.\n' "$alib" 1>&2
+          exit 1
+      fi
+      sudo mkdir -p ./rootfs"${d}"
+      echo -- cp -aL "${d}${alib}" ./rootfs"${d}${alib}"
+      sudo cp -aL "${d}${alib}" ./rootfs"${d}${alib}"
+  done
+
+rm $unsorted
+
+## Test via chroot to ensure all requisite libraries are present.
+#sudo chroot rootfs/ /bin/bash
+
+## Copy the portable binary distribution into the image.
+#rsync -a /tmp/portable_dcma/ ./rootfs/portable_dcma/
 
 rm initramfs.cpio.gz || true
 cd rootfs
@@ -109,3 +188,4 @@ qemu-system-x86_64 \
   -kernel bzImage \
   -initrd initramfs.cpio.gz \
   --append "root=/dev/ram0 rootfstype=ramfs init=/init console=ttyS0"
+
