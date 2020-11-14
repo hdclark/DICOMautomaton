d6e04bbebf762e990744418d34c765f6fde625ea
Cailin <cailin.ringstrom@gmail.com>
2020-11-13 22:27:14 -0800

    Added unit testing framework

---
+++ registration/unit_tests/Main.cc
@@ -0,0 +1,5 @@
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
+#include "doctest/doctest.h"
+
---
+++ registration/unit_tests/compile_and_run.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+set -eu
+
+# Find root of repository
+export REPOROOT=$(git rev-parse --show-toplevel || true)
+if [ ! -d "${REPOROOT}" ] ; then
+    printf 'Unable to find git repo root. Refusing to continue.\n' 1>&2
+    exit 1
+fi
+
+export SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}" )" )"
+if [ ! -d "${SCRIPT_DIR}" ] ; then
+    printf 'Unable to find script directory. Refusing to continue.\n' 1>&2
+    exit 1
+fi
+cd "${SCRIPT_DIR}"
+
+# Ensure the doctest header is available.
+if [ ! -f doctest/doctest.h ] ; then
+    mkdir -p doctest/
+    wget -q 'https://raw.githubusercontent.com/onqtam/doctest/master/doctest/doctest.h' -O doctest/doctest.h
+fi
+
+g++ -std=c++17 -Wall -I. -I"${REPOROOT}/src" \
+  Main.cc \
+  {,"${REPOROOT}/registration/unit_tests/"}CPD_Tests.cc \
+  -o run_tests \
+  -pthread \
+  -lboost_system \
+  -lygor
+
+./run_tests #--success
+
+rm -rf doctest/
+rm ./run_tests
+
