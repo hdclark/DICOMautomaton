4b6868a44419caebc6c992abae01d536130a9b83
carlosjdoebeli <carlosjdoebeli@gmail.com>
2020-11-14 18:31:28 -0800

    Edited to compile

--- registration/src/CPD_Shared.cc
+++ registration/src/CPD_Shared.cc
@@ -1,13 +1,16 @@
 #include "CPD_Shared.h"
 
-Eigen::MatrixXd E_Step(const Eigen::MatrixXd & xPoints
-            const Eigen::MatrixXd & yPoints
-            const Eigen::MatrixXd & BRMatrix) {
+Eigen::MatrixXd E_Step(const Eigen::MatrixXd & xPoints,
+            const Eigen::MatrixXd & yPoints,
+            const Eigen::MatrixXd & BRMatrix,
+            const Eigen::MatrixXd & t,
+            double sigma,
+            double w,
+            double mRowsY,
+            double nRowsX,
+            double dimensionality) {
 
-    Eigen::MatrixXd postProb(yPoints.rows(), xPoints.rows()) = Eigen::MatrixXd::Zero(yPoints.rows(),xPoints.rows());
-    int dimensionality = xPoints.cols();
-    int nRowsX = (int)xPoints.rows();
-    int mRowsY = (int)yPoints.rows();
+    Eigen::MatrixXd postProb = Eigen::MatrixXd::Zero(yPoints.rows(),xPoints.rows());
 
     Eigen::MatrixXd tempVector;
     double expArg;
@@ -19,21 +22,21 @@ Eigen::MatrixXd E_Step(const Eigen::MatrixXd & xPoints
         for (size_t n = 0; n < nRowsX; ++n) {
 
             tempVector = xPoints.row(n) - (BRMatrix * yPoints.row(m) + t);
-            expArg = - 1 / (2 * pow(sigma, 2)) * pow(vector.norm(),2);
+            expArg = - 1 / (2 * pow(sigma, 2)) * pow(tempVector.norm(),2);
             numerator = exp(expArg);
             
             denomSum = 0;
             
             for (size_t k = 0; k < mRowsY; ++k) {
                 tempVector = xPoints.row(n) - (BRMatrix * yPoints.row(k) + t);
-                expArg = - 1 / (2 * pow(sigma, 2)) * pow(vector.norm(),2);
+                expArg = - 1 / (2 * pow(sigma, 2)) * pow(tempVector.norm(),2);
                 denomSum += exp(expArg);
             }
 
             // Check if there is an issue with pi using math.h
-            denominator = denomSum + pow(2 * M_PI * pow(sigma, 2),(D/2)) * (w/(1-w)) * M / N;
+            denominator = denomSum + pow(2 * M_PI * pow(sigma, 2),((double)(dimensionality/2))) * (w/(1-w)) * (double)(mRowsY / nRowsX);
 
-            PMatrix(m,n) = numerator / denominator;
+            postProb(m,n) = numerator / denominator;
 
         }
     }
