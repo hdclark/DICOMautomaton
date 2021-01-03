a7cfeb925fadf1b21da38ff758f10bb3ad38df8f
Julia Rosenrauch <jul_rose@yahoo.ca>
2021-01-03 10:46:46 -0800

    ok one more

--- registration/unit_tests/CPD_Tests.cc
+++ registration/unit_tests/CPD_Tests.cc
@@ -404,7 +404,7 @@ TEST_CASE("Update SS non-rigid") {
 				2, 2, 2, 2;
 	Eigen::MatrixXd transformedPoints(4, 3);
 	transformedPoints << 1, 1, 1,
-						 2, 2, 2;
+						 2, 2, 2,
 						 1, 2, 3,
 						 3, 2, 1;
 	double sigmaSquared = SigmaSquared(xPoints, postProb, transformedPoints);
