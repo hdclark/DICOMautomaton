b321b8c17263c4a46e87de61ff2f8fa6e04f9e62
jacksongayda <jacksongayda@gmail.com>
2021-01-21 16:09:38 -0800

    Create a script that compiles and installs, generates the test inputs, and runs the model

---
+++ sycl/compile_and_run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+read -p "Running this script will replace all files in DICOMautomaton/sycl/data are you sure you want to continue? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    cd /DICOMautomaton/sycl/
+    bash ./compile_and_install.sh 
+    cd data/
+    rm -r *
+    gen_test_inputs
+    run_model -a aif.txt -v vif.txt -c c.txt
+fi
+
