00d65d3a7ffb5876b630481663d20c105198bfd5
Cailin <cailin.ringstrom@gmail.com>
2021-02-18 12:06:48 -0800

    Updated image plotting script

--- registration/helper_scripts/image_plotter.py
+++ registration/helper_scripts/image_plotter.py
@@ -23,8 +23,11 @@ def read_file(file_name):
 def plot_points(x1,y1,z1,x2,y2,z2):
     fig = plt.figure()
     ax1 = fig.add_subplot(111, projection = '3d')
-    ax1.scatter(x1, y1, z1, s=10, c='b', marker="s", label='Original')
-    ax1.scatter(x2, y2, z2, s=10, c='r', marker="o", label='Translated')
+    ax1.scatter(x1, y1, z1, s=1, c='b', marker="s", label='Stationary Dataset')
+    ax1.scatter(x2, y2, z2, s=1, c='r', marker="o", label='Moving Dataset')
+    ax1.set_axis_off()
+    ax1.view_init(130, -130)
+    # ax1.view_init(-80, 100) #anteater
     plt.legend(loc='upper left')
     plt.show()
 
