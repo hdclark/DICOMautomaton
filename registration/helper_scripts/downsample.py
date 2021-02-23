47f12f6fc8f1ea31b74133ef3b882cd4b70f89d7
Cailin <cailin.ringstrom@gmail.com>
2021-02-22 16:18:49 -0800

    Changed to downsample corresponding sets

--- registration/helper_scripts/downsample.py
+++ registration/helper_scripts/downsample.py
@@ -12,13 +12,13 @@ def read_file(file_name):
 
     return np.array(points)
 
-def random_downsample(full_point_set, percentage):
-    num_points = full_point_set.shape[0]
+def random_downsample(full_point_set1, full_point_set2, percentage):
+    num_points = full_point_set1.shape[0]
     num_points_to_keep = int((1-percentage) * num_points)
     indices = np.arange(num_points)
     indices_to_keep = np.random.choice(
         indices, size=num_points_to_keep, replace=False)
-    return full_point_set[indices_to_keep]
+    return full_point_set1[indices_to_keep], full_point_set2[indices_to_keep]
 
 def write_file(file_name, point_set):
     with open(file_name, "w") as fp:
@@ -29,15 +29,20 @@ def write_file(file_name, point_set):
 def main():
     parser = argparse.ArgumentParser(
         description="File I/O specification for plotter")
-    parser.add_argument("infile", help="Path to moving point cloud files")
+    parser.add_argument("infile1", help="Path to first point cloud files")
+    parser.add_argument("infile2", help="Path to second point cloud files")
     parser.add_argument(
         "ds_rate", help="Percentage of points to remove", type=float)
-    parser.add_argument("outfile", help="Path to save file")
+    parser.add_argument("outfile1", help="Path to save file")
+    parser.add_argument("outfile2", help="Path to save file")
 
     args = parser.parse_args()
-    full_point_set = read_file(args.infile)
-    downsampled_point_set = random_downsample(full_point_set, args.ds_rate)
-    write_file(args.outfile, downsampled_point_set)
+    full_point_set1 = read_file(args.infile1)
+    full_point_set2 = read_file(args.infile2)
+    downsampled_point_set1, downsampled_point_set2 = \
+        random_downsample(full_point_set1, full_point_set2, args.ds_rate)
+    write_file(args.outfile1, downsampled_point_set1)
+    write_file(args.outfile2, downsampled_point_set2)
 
 if __name__ == '__main__':
     main()
