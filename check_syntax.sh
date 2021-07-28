8fc7780e91aae2c87b3e6b0a468fedcae8853edf
Haley Clark <gmail.com@hdeanclark>
2021-07-28 12:34:43 -0600

    check_syntax: optionally check YAML files.

--- check_syntax.sh
+++ check_syntax.sh
@@ -34,13 +34,27 @@ export -f check_cpp_syntax
 
 check_sh_syntax () {
     local f="$*"
-    [ -f "$f" ] && {
-      shellcheck \
-        -e SC1117,SC2059 \
-        "$f"
-    }
+    if [ -f "$f" ] && type "shellcheck" &> /dev/null ; then
+        shellcheck \
+          -e SC1117,SC2059 \
+          "$f"
+    else
+        printf "'shellcheck' not available, so skipping shell script check for '%s'\n" "$f"
+    fi
 }
 export -f check_sh_syntax
+
+check_yml_syntax () {
+    local f="$*"
+    if [ -f "$f" ] && type "yamllint" &> /dev/null ; then
+        yamllint \
+          -d "{extends: relaxed, rules: {line-length: {max: 200}}}" \
+          "$f"
+    else
+        printf "'yamllint' not available, so skipping YAML file check for '%s'\n" "$f"
+    fi
+}
+export -f check_yml_syntax
 
 # Check all files in the project.
 #find ./src/ -type f -print0 |
@@ -54,14 +68,19 @@ export -f check_sh_syntax
 
 # Check only modified and untracked files.
 git ls-files -z -o -m "$@" |
-  grep -z -E '*[.]h|*[.]cc|*[.]cpp' |
+  grep -z -E '.*[.]h|.*[.]cc|.*[.]cpp' |
     `# Ignore imebra headers. Useful for spelunking and testing... ` \
     `# grep -z -i -v '.*imebra.*' |   ` \
   xargs -0 -I '{}' -P "$(nproc || echo 2)" -n 1 -r \
     bash -c "check_cpp_syntax '{}'"
 
 git ls-files -z -o -m "$@" |
-  grep -z -E '*[.]sh' |
+  grep -z -E '.*[.]sh' |
   xargs -0 -I '{}' -P 1 -n 1 -r \
     bash -c "check_sh_syntax '{}'"
+
+git ls-files -z -o -m "$@" |
+  grep -z -E '.*[.]yml|.*[.]yaml' |
+  xargs -0 -I '{}' -P 1 -n 1 -r \
+    bash -c "check_yml_syntax '{}'"
 
