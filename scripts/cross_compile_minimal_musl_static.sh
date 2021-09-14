36b180b3d7d98f4bf410cbfc830fb74775979dab
Haley Clark <gmail.com@hdeanclark>
2021-09-14 13:12:46 -0700

    Cross-compiling: switch back to zig-based compiler.
    The musl toolchain works perfectly well for linux static compilation, but the zig toolchain support many more systems
    and painlessly supports glibc version-pinning. The current script generates mostly-static binaries that only require
    glibc 2.18, which is sufficiently ancient for all currently conceivable situations.

--- scripts/cross_compile_minimal_musl_static.sh
+++ scripts/cross_compile_minimal_musl_static.sh
@@ -9,61 +9,55 @@
 # At the moment only a minimum set of dependecies are supported.
 # Ideas and suggestions are welcome!
 
-
 # sudo docker run --rm -it debian:oldstable
+
 set -eux
+
+# See https://github.com/richfelker/musl-cross-make README (musl cross-compiler).
+#export TRIPLET='arm-linux-musleabi'
+# See `zig target` (libc section) for other supported triplets using zig toolchain.
+#export TRIPLET='x86_64-linux-musl'
+#export TRIPLET='x86_64-linux-gnu.2.28' # Debian oldstable as of 20210914.
+export TRIPLET='x86_64-linux-gnu.2.18' # Ancient glibc... Seems to have __cxa_thread_atexit_impl@GLIBC_2.18 whereas 2.17 does not have __cxa_thread_atexit_impl
 
 apt-get update -y
 apt-get upgrade -y
-apt-get install -y clang g\+\+ wget xz-utils cmake git file vim rsync openssh-client \
+apt-get install -y clang wget xz-utils cmake git file vim rsync openssh-client \
                    python3 unzip #python3-pip pkg-config autoconf
 
-## Zig-based toolchain.
-#cd
-#wget 'https://ziglang.org/builds/zig-linux-x86_64-0.9.0-dev.952+0c091feb5.tar.xz'
-#tar -axf zig-linux-x86_64-0.9.0-dev.952+0c091feb5.tar.xz 
-#rm zig-linux-x86_64-0.9.0-dev.952+0c091feb5.tar.xz 
-#cd zig-linux-x86_64-0.9.0-dev.952+0c091feb5/
-#
-#export CC="$(pwd)/zig cc --target=x86_64-linux-musl -static -fno-use-cxa-atexit"
-#export CXX="$(pwd)/zig c++ --target=x86_64-linux-musl -static -fno-use-cxa-atexit"
-#export CXXFLAGS='-I/musl/include/ -I/musl/usr/include/ -L/musl/lib/ -L/musl/usr/lib/ -static -fno-use-cxa-atexit'
-#
-#
-## Musl gcc wrapper toolchain.
-#cd
-#rm -rf musl-*
-#wget 'http://musl.libc.org/releases/musl-1.2.2.tar.gz'
-#tar -axf 'musl-1.2.2.tar.gz'
-#cd musl-1.2.2/
-#unset CC CXX CXXFLAGS
-#CC="clang" CXX="clang++" ./configure --prefix=/musl --disable-shared --enable-wrapper
+# Zig-based toolchain.
+cd / && rm -rf /zig*
+wget 'https://ziglang.org/builds/zig-linux-x86_64-0.9.0-dev.952+0c091feb5.tar.xz'
+tar -axf zig-linux-x86_64-0.9.0-dev.952+0c091feb5.tar.xz 
+cd /zig*/
+unset CC CXX CFLAGS CXXFLAGS LDFLAGS
+export CC="$(pwd)/zig cc --verbose --target=${TRIPLET}"
+export CXX="$(pwd)/zig c++ --verbose --target=${TRIPLET}"
+export CFLAGS='-I/pot/include/ -I/pot/usr/include/ -L/pot/lib/ -L/pot/usr/lib/ -static'
+export CXXFLAGS='-I/pot/include/ -I/pot/usr/include/ -L/pot/lib/ -L/pot/usr/lib/ -static -fno-use-cxa-atexit'
+export LDFLAGS=""
+cd /
+
+
+## Musl (linux-only) cross-compiler.
+#git clone 'https://github.com/richfelker/musl-cross-make' /mcm
+#cd /mcm
+#unset CC CXX CFLAGS CXXFLAGS LDFLAGS
+#export CC="clang"
+#export CXX="clang++"
+#export CFLAGS=""
+#export CXXFLAGS=""
+#export LDFLAGS=""
+#printf '\nTARGET=%s\n' "$TRIPLET" >> config.mak
+#printf '\n%s\n' 'OUTPUT=/musl' >> config.mak
+##printf '\n%s\n' 'GCC_VER = 10.2.0' >> config.mak
 #time make -j8 VERBOSE=1 install
-##export CC="/musl/bin/musl-gcc -static"
-##export CXX="/musl/bin/musl-gcc -static"
-#export CC="/musl/bin/musl-clang -static"
-#export CXX="/musl/bin/musl-clang -static"
-#export CXXFLAGS="-I/musl/include/ -I/musl/usr/include/ -L/musl/lib/ -L/musl/usr/lib/ -static"
-#rm -rf musl-*
-
-
-# Musl cross-compiler.
-git clone 'https://github.com/richfelker/musl-cross-make' /mcm
-cd /mcm
-unset CC CXX CXXFLAGS LDFLAGS
-export CC="clang"
-export CXX="clang++"
-export LDFLAGS=""
-printf '\n%s\n' 'TARGET=x86_64-linux-musl' >> config.mak
-printf '\n%s\n' 'OUTPUT=/musl' >> config.mak
-#printf '\n%s\n' 'GCC_VER = 10.2.0' >> config.mak
-time make -j8 VERBOSE=1 install
-
-unset CC CXX CXXFLAGS LDFLAGS
-export CC="/musl/bin/x86_64-linux-musl-gcc"
-export CXX="/musl/bin/x86_64-linux-musl-g++"
-export CXXFLAGS="-I/musl/include/ -I/musl/usr/include/ -L/musl/lib/ -L/musl/usr/lib/ -static -fno-use-cxa-atexit"
-export LDFLAGS=""
+#unset CC CXX CFLAGS CXXFLAGS LDFLAGS
+#export CC="/musl/bin/${TRIPLET}-gcc"
+#export CXX="/musl/bin/${TRIPLET}-g++"
+#export CXXFLAGS="-I/pot/include/ -I/pot/usr/include/ -L/pot/lib/ -L/pot/usr/lib/ -static -fno-use-cxa-atexit"
+#export CFLAGS="-I/pot/include/ -I/pot/usr/include/ -L/pot/lib/ -L/pot/usr/lib/ -static"
+#export LDFLAGS=""
 
 
 # zlib.
@@ -72,8 +66,8 @@ export LDFLAGS=""
     wget 'https://zlib.net/zlib-1.2.11.tar.gz'
     tar -axf 'zlib-1.2.11.tar.gz'
     rm 'zlib-1.2.11.tar.gz'
-    cd zlib-1.2.11
-    ./configure --static --prefix=/musl
+    cd /zlib-1.2.11
+    ./configure --static --prefix=/pot
     time make -j8 VERBOSE=1 install
     cd / && rm -rf /zlib*
 )
@@ -86,24 +80,24 @@ export LDFLAGS=""
     rm 'boost_1_76_0.tar.gz' 
     cd /boost_1_76_0/tools/build/src/engine/
     printf '%s\n' "using gcc : : ${CXX} : <cxxflags>${CXXFLAGS} <linkflags>${LDFLAGS} ;" > /root/user-config.jam
-    printf '%s\n' "using zlib : 1.2.11 : <search>/musl/lib <name>zlib <include>/musl/include ;" >> /root/user-config.jam
+    printf '%s\n' "using zlib : 1.2.11 : <search>/pot/lib <name>zlib <include>/pot/include ;" >> /root/user-config.jam
     ./build.sh --cxx="clang++"
     cd ../../
     ./bootstrap.sh --cxx="clang++"
-    ./b2 --prefix=/musl install
+    ./b2 --prefix=/pot install
     cd ../../
     ./tools/build/src/engine/b2 -j8 \
         --user-config=/root/user-config.jam \
-        --prefix=/musl \
+        --prefix=/pot \
         release \
         toolset=gcc \
         debug-symbols=off \
         threading=multi \
         runtime-link=static \
         link=static \
-        cflags=-fno-strict-aliasing \
-        --includedir=/musl/usr/include \
-        --libdir=/musl/usr/lib \
+        cflags='-fno-use-cxa-atexit -fno-strict-aliasing' \
+        --includedir=/pot/usr/include \
+        --libdir=/pot/usr/lib \
         --with-iostreams \
         --with-serialization \
         --with-thread \
@@ -114,10 +108,10 @@ export LDFLAGS=""
         --with-date_time \
         --with-atomic \
         -sZLIB_NAME=libz \
-        -sZLIB_INCLUDE=/musl/include/ \
-        -sZLIB_LIBRARY_PATH=/musl/lib/ \
+        -sZLIB_INCLUDE=/pot/include/ \
+        -sZLIB_LIBRARY_PATH=/pot/lib/ \
         install
-    ls -lash /musl/usr/lib
+    ls -lash /pot/usr/lib
     cd / && rm -rf /boost*
 )
 
@@ -129,7 +123,7 @@ export LDFLAGS=""
     wget 'https://sourceforge.net/projects/asio/files/latest/download' -O asio.tgz
     tar -axf asio.tgz || unzip asio.tgz
     cd asio-*/
-    cp -v -R include/asio/ include/asio.hpp /musl/include/
+    cp -v -R include/asio/ include/asio.hpp /pot/include/
     cd / && rm -rf /asio
 )
 
@@ -173,7 +167,7 @@ export LDFLAGS=""
 #    tar -axf SDL.tgz
 #    cd SDL*/
 #    ./configure \
-#        --prefix=/musl \
+#        --prefix=/pot \
 #        --enable-static \
 #        --disable-shared \
 #        \
@@ -201,7 +195,7 @@ export LDFLAGS=""
 #        --disable-video-x11-xinerama \
 #        --disable-video-x11-xrandr \
 #        --disable-video-x11-scrnsaver \
-#        --with-sysroot=/musl \
+#        --with-sysroot=/pot \
 #        --with-x
 #    time make -j8 VERBOSE=1 install
 #    cd / && rm -rf /sdl
@@ -214,7 +208,7 @@ export LDFLAGS=""
     git clone --depth 1 https://github.com/hdclark/YgorClustering.git /ygorclustering
     cd /ygorclustering/
     cmake -B build \
-        -DCMAKE_INSTALL_PREFIX='/musl' \
+        -DCMAKE_INSTALL_PREFIX='/pot' \
         -DCMAKE_BUILD_TYPE=Release \
         -DWITH_LINUX_SYS=OFF \
         -DWITH_EIGEN=OFF \
@@ -234,7 +228,7 @@ export LDFLAGS=""
     git clone --depth 1 https://github.com/hdclark/Explicator.git /explicator
     cd /explicator/
     cmake -B build \
-        -DCMAKE_INSTALL_PREFIX='/musl' \
+        -DCMAKE_INSTALL_PREFIX='/pot' \
         -DCMAKE_BUILD_TYPE=Release \
         -DWITH_LINUX_SYS=OFF \
         -DWITH_EIGEN=OFF \
@@ -245,7 +239,7 @@ export LDFLAGS=""
     cd build/
     time make -j8 VERBOSE=1 install
     cd / && rm -rf /explicator
-    file /musl/bin/explicator_cross_verify
+    file /pot/bin/explicator_cross_verify
 )
 
 
@@ -255,19 +249,19 @@ export LDFLAGS=""
     git clone --depth 1 https://github.com/hdclark/Ygor.git /ygor
     cd /ygor
     cmake -B build \
-        -DCMAKE_INSTALL_PREFIX='/musl' \
+        -DCMAKE_INSTALL_PREFIX='/pot' \
         -DCMAKE_BUILD_TYPE=Release \
         -DWITH_LINUX_SYS=OFF \
         -DWITH_EIGEN=OFF \
         -DWITH_GNU_GSL=OFF \
         -DWITH_BOOST=ON \
         -DBUILD_SHARED_LIBS=OFF \
-        -DBOOST_INCLUDEDIR=/musl/usr/include/ \
+        -DBOOST_INCLUDEDIR=/pot/usr/include/ \
         .
     cd build
     time make -j8 VERBOSE=1 install
     cd / && rm -rf /ygor
-    file /musl/bin/regex_tester
+    file /pot/bin/regex_tester
 )
 
 
@@ -276,16 +270,21 @@ export LDFLAGS=""
     cd / && rm -rf /dcma
     git clone --depth 1 https://github.com/hdclark/DICOMautomaton.git /dcma
     cd /dcma
-    # Drop mpfr and gmp (not needed for musl toolchains).
+
+    # Drop mpfr and gmp (not needed for musl toolchains, seemingly not needed for static compilation either).
     sed -i -e 's/.*mpfr.*//g' -e 's/.*gmp.*//g' src/CMakeLists.txt 
+
     # Implement a bogus thread dtor routine, just to make compilation succeed (runtime be damned!)
+    #
+    # Note: not recommended, unless absolutely required, for obvious reasons.
     #printf '\n\n%s\n\n' 'extern "C" int __cxa_thread_atexit_impl(void (*)(), void *, void *){ return 0; }' >> src/DICOMautomaton_Dispatcher.cc
+
     cmake -B build \
-        -DCMAKE_INSTALL_PREFIX='/musl' \
+        -DCMAKE_INSTALL_PREFIX='/pot' \
         -DCMAKE_BUILD_TYPE=Release \
         -DBUILD_SHARED_LIBS=OFF \
         -DDCMA_VERSION=dcma_manual_test_version \
-        -DCMAKE_INSTALL_SYSCONFDIR=/musl/etc \
+        -DCMAKE_INSTALL_SYSCONFDIR=/pot/etc \
         -DMEMORY_CONSTRAINED_BUILD=OFF \
         -DWITH_LINUX_SYS=OFF \
         -DWITH_ASAN=OFF \
@@ -301,17 +300,18 @@ export LDFLAGS=""
         -DWITH_GNU_GSL=OFF \
         -DWITH_POSTGRES=OFF \
         -DWITH_JANSSON=OFF \
-        -DBOOST_INCLUDEDIR=/musl/usr/include/ \
-        -DBOOST_LIBRARYDIR=/musl/usr/lib/ \
-        -DBOOST_ROOT=/musl/ \
+        -DBOOST_INCLUDEDIR=/pot/usr/include/ \
+        -DBOOST_LIBRARYDIR=/pot/usr/lib/ \
+        -DBOOST_ROOT=/pot/ \
         .
     cd build
     time make -j8 VERBOSE=1 install
     cd / && rm -rf /dcma
-    file /musl/bin/dicomautomaton_dispatcher
+    file /pot/bin/dicomautomaton_dispatcher
+    strings /pot/bin/dicomautomaton_dispatcher | grep GLIBC | sort --version-sort | uniq || true
 )
 
-# ldd for musl ...
+# ldd for pot ...
 #apt-get install -y musl-dev
-#/lib/ld-musl-x86_64.so.1 --list /musl/bin/dicomautomaton_dispatcher
+#/lib/ld-musl-x86_64.so.1 --list /pot/bin/dicomautomaton_dispatcher
 
