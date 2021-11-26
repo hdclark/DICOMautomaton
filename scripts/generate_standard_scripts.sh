272dc71cc392fc2d5f9e655c00bab514fe334742
Hal Clark <haley.clark@bccancer.bc.ca>
2021-11-26 11:35:15 -0800

    Make script imbuing more robust.
    There doesn't seem to be a universal hex dump utility (maybe 'od', but not sure). This commit adds some resilience if
    'hexdump' isn't found, attempting a few others that might be available. Also, it now loudly fails when none are found.

--- scripts/generate_standard_scripts.sh
+++ scripts/generate_standard_scripts.sh
@@ -3,6 +3,8 @@
 # This script bundles the standard DCMA scripts into a C++ file so they are always available when distributing DCMA.
 # This script should be called from the root directory.
 
+set -eux
+
 # Handle spaces in the input and output file paths.
 in_file="$1"
 shift
@@ -12,24 +14,53 @@ while [ ! -f "$in_file" ] ; do
 done
 out_file="$@"
 
+function command_exists () {
+    type "$1" &> /dev/null ;
+}
+export -f command_exists
+
+if ! command_exists grep ; then
+    printf 'This script requires grep.' 1>&2
+    exit 1
+fi
+if ! command_exists tr ; then
+    printf 'This script requires tr.' 1>&2
+    exit 1
+fi
+if ! command_exists sed ; then
+    printf 'This script requires sed.' 1>&2
+    exit 1
+fi
+
+# Convert the scripts to entries in a C++-style initializer list.
+# To simplify inclusion, we convert the contents to hex data and embed in a string.
 inline_scripts=""
-printf '==============================================\n'
-printf '==============================================\n'
-printf '==============================================\n'
-
-pwd
-
 for script in ./artifacts/dcma_scripts/*/*.dscr ; do
     script_path="${script}"
+
     # Note: the following may be more portable, but doesn't seem to work. Need to continue tweaking it...
     #script_contents="$( while read -n 1 c ; do printf '\\x%02x' "'${c}" ; done < "${script_path}" )"
-    script_contents="$( hexdump -ve '1/1 "%02x"' "${script_path}" | sed -e 's@\([0-9a-fA-F][0-9a-fA-F]\)@\\x\1@g' )"
-    script_category="$(printf '%s' "${script_path}" | sed -e 's@.*/\(.*\)/[^/]*@\1@g' | tr '_' ' ' )"
-    script_filename="$(printf '%s' "${script_path}" | sed -e 's@.*/\(.*\)@\1@g' -e 's@[.]dscr$@@g' | tr '_' ' ' )"
+    if command_exists hexdump ; then
+        raw_script_contents="$( cat "${script_path}" | hexdump -ve '1/1 "%02x"' )" 
+    elif command_exists od ; then
+        raw_script_contents="$( cat "${script_path}" | od -v -t x1 -An | tr -d "\n "~ )" 
+    elif command_exists xxd ; then
+        raw_script_contents="$( cat "${script_path}" | xxd -p | tr -d '\n' )" 
+    else
+        printf 'Unable to find suitable hex dump utility.' 1>&2
+        exit 1
+    fi
+
+    # Add C/C++-style '\x' prefix so it can be included into a C++ source file.
+    script_contents="$( printf '%s' "${raw_script_contents}" | sed -e 's@\([0-9a-fA-F][0-9a-fA-F]\)@\\x\1@g' )"
+
+    script_category="$( printf '%s' "${script_path}" | sed -e 's@.*/\(.*\)/[^/]*@\1@g' | tr '_' ' ' )"
+    script_filename="$( printf '%s' "${script_path}" | sed -e 's@.*/\(.*\)@\1@g' -e 's@[.]dscr$@@g' | tr '_' ' ' )"
 
     inline_scripts="${inline_scripts} {\"${script_category}\",\"${script_filename}\",\"${script_contents}\"},"
 done
 
+# Filter the input file, emitting the stringified script data into the output file when a bare C++ macro is observed.
 printf '' > "${out_file}"
 while IFS= read -u 9 -r aline ; do
     if [ ! -z "$( printf '%s' "$aline" | grep '^[ ]*DCMA_SCRIPT_INLINES[ ]*' )" ] ; then
@@ -39,54 +70,4 @@ while IFS= read -u 9 -r aline ; do
         printf '%s\n' "${aline}" >> "${out_file}"
     fi
 done 9< "${in_file}"
-wc -l "${in_file}" "${out_file}"
-
-#printf '%s' "${inline_scripts}" > /tmp/dcma_inline_scripts.cc
-#cp "${out_file}" /tmp/dcma_out_file.cc
-#
-#cat "${in_file}" |
-#    sed -e "s@[[:space:]]*DCMA_SCRIPT_INLINES@${inline_scripts}@g" > "${out_file}"
-
-
-printf '==============================================\n'
-printf '==============================================\n'
-printf '==============================================\n'
-
-exit
-
-
-Script_Loader.cc Standard_Scripts
-
-set(script_inlines "")
-function(embed_script
-         filename)
-    # Read the file contents.
-    file(READ "${filename}" script_contents HEX)
-    #message(AUTHOR_WARNING "${script_contents}")
-
-    # Derive a name for the script from the filename.
-    set(script_path "${filename}")
-    string(REGEX REPLACE ".*/(.*)/[^/]*" "\\1" script_category "${script_path}")
-    string(REGEX REPLACE "_" " " script_category "${script_category}")
-
-    string(REGEX REPLACE ".*/(.*)" "\\1" script_filename "${script_path}")
-    string(REGEX REPLACE "_" " " script_filename "${script_filename}")
-    string(REGEX REPLACE "[.]dscr$" "" script_filename "${script_filename}")
-
-    # Convert the file contents to a C++ string with hex escapes.
-    string(REGEX REPLACE "([0-9a-f][0-9a-f])" "\\\\x\\1" script_contents "${script_contents}")
-    #message(AUTHOR_WARNING "${script_contents}")
-
-    set(entry "{\"${script_category}\",\"${script_filename}\",\"${script_contents}\"},")
-
-    # Append the string to a variable for later access.
-    set(script_inlines "${script_inlines}" PARENT_SCOPE)
-    set(script_inlines "${script_inlines}${entry}" PARENT_SCOPE)
-endfunction()
-
-file(GLOB script_files "../artifacts/dcma_scripts/*/*.dscr")
-foreach(script_file ${script_files})
-    embed_script("${script_file}")
-endforeach()
-#message(AUTHOR_WARNING "${script_inlines}")
 
