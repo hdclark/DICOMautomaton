39fe4e8c7f7594ce48cbc01ae1dc82131daa7661
Hal Clark <haley.clark@bccancer.bc.ca>
2021-12-16 10:04:43 -0800

    Add minimal 'table' class.
    Note that this commit does not yet expose the table class in any way.

---
+++ src/Tables.cc
@@ -0,0 +1,298 @@
+
+#include <set>
+#include <string>
+#include <iostream>
+#include <limits>
+#include <utility>
+
+#include "Tables.h"
+
+//#ifndef DCMA_TABLE_DISABLE_ALL_SPECIALIZATIONS
+//    #define DCMA_TABLE_DISABLE_ALL_SPECIALIZATIONS
+//#endif
+
+namespace tables {
+
+// cell class.
+
+template <class T>
+cell<T>::cell() : row(-1), col(-1) {}
+#ifndef DCMA_TABLE_DISABLE_ALL_SPECIALIZATIONS
+    template cell<double     >::cell();
+    template cell<int64_t    >::cell();
+    template cell<std::string>::cell();
+#endif
+
+template <class T>
+cell<T>::cell(int64_t r, int64_t c, const T& v) : row(r), col(c), val(v) {}
+#ifndef DCMA_TABLE_DISABLE_ALL_SPECIALIZATIONS
+    template cell<double     >::cell(int64_t, int64_t, const double     &);
+    template cell<int64_t    >::cell(int64_t, int64_t, const int64_t    &);
+    template cell<std::string>::cell(int64_t, int64_t, const std::string&);
+#endif
+
+template <class T>
+bool
+cell<T>::operator==(const cell& rhs) const {
+    return (this->row == rhs.row) && (this->col == rhs.col);
+}
+#ifndef DCMA_TABLE_DISABLE_ALL_SPECIALIZATIONS
+    template bool cell<double     >::operator==(const cell&) const;
+    template bool cell<int64_t    >::operator==(const cell&) const;
+    template bool cell<std::string>::operator==(const cell&) const;
+#endif
+
+template <class T>
+bool
+cell<T>::operator!=(const cell& rhs) const {
+    return !(*this == rhs);
+}
+#ifndef DCMA_TABLE_DISABLE_ALL_SPECIALIZATIONS
+    template bool cell<double     >::operator!=(const cell&) const;
+    template bool cell<int64_t    >::operator!=(const cell&) const;
+    template bool cell<std::string>::operator!=(const cell&) const;
+#endif
+
+template <class T>
+bool
+cell<T>::operator<(const cell& rhs) const {
+    // Make the primary sorting axis the row number.
+    // This makes it easier to append new rows, which is expected to happen more
+    // frequently than appending new columns.
+    return (this->row == rhs.row) ? (this->col < rhs.col)
+                                  : (this->row < rhs.row);
+}
+#ifndef DCMA_TABLE_DISABLE_ALL_SPECIALIZATIONS
+    template bool cell<double     >::operator<(const cell&) const;
+    template bool cell<int64_t    >::operator<(const cell&) const;
+    template bool cell<std::string>::operator<(const cell&) const;
+#endif
+
+template <class T>
+int64_t
+cell<T>::get_row() const {
+    return this->row;
+}
+#ifndef DCMA_TABLE_DISABLE_ALL_SPECIALIZATIONS
+    template int64_t cell<double     >::get_row() const;
+    template int64_t cell<int64_t    >::get_row() const;
+    template int64_t cell<std::string>::get_row() const;
+#endif
+
+template <class T>
+int64_t
+cell<T>::get_col() const {
+    return this->col;
+}
+#ifndef DCMA_TABLE_DISABLE_ALL_SPECIALIZATIONS
+    template int64_t cell<double     >::get_col() const;
+    template int64_t cell<int64_t    >::get_col() const;
+    template int64_t cell<std::string>::get_col() const;
+#endif
+
+
+// table2 class.
+
+table2::table2(){};
+
+std::pair<int64_t, int64_t>
+table2::min_max_row() const {
+    int64_t min = std::numeric_limits<int64_t>::max();
+    int64_t max = std::numeric_limits<int64_t>::min();
+    if(const auto it = std::rbegin(this->data); it != std::rend(this->data)){
+        max = it->get_row();
+    }
+    if(const auto it = std::begin(this->data); it != std::end(this->data)){
+        min = it->get_row();
+    }
+    if(max < min){
+        throw std::runtime_error("No data available, min and max rows are not defined");
+    }
+    return {min, max};
+}
+
+std::pair<int64_t, int64_t>
+table2::min_max_col() const {
+    int64_t min = std::numeric_limits<int64_t>::max();
+    int64_t max = std::numeric_limits<int64_t>::min();
+
+    for(const auto& c : this->data){
+        const auto col = c.get_col();
+        max = std::max(max, col);
+        min = std::min(min, col);
+    }
+    if(max < min){
+        throw std::runtime_error("No data available, min and max columns are not defined");
+    }
+    return {min, max};
+}
+
+std::optional<std::reference_wrapper<std::string>>
+table2::value(int64_t row, int64_t col){
+    std::optional<std::reference_wrapper<std::string>> out;
+    auto it = this->data.find( cell<std::string>(row, col, "") );
+    if(it != std::end(this->data)){
+        // NOTE: std::set does not care about the cell's content, but doesn't know this.
+        // So we have to cast away the const on the value. Do NOT cast away the const
+        // on the row or column numbers!
+        out = std::ref(const_cast<std::string&>(it->val));
+    }
+    return out;
+}
+
+int64_t
+table2::next_empty_row() const {
+    int64_t out = 0;
+    if(const auto it = std::rbegin(this->data); it != std::rend(this->data)){
+        out = it->get_row() + 1;
+    }
+    return out;
+}
+
+int64_t
+table2::next_empty_col() const {
+    int64_t out = 0;
+    for(const auto& c : this->data){
+        out = std::max(out, c.get_col() + 1);
+    }
+    return out;
+}
+
+void
+table2::inject(int64_t row, int64_t col, const std::string& val){
+    cell<std::string> n(row, col, val);
+    auto it = this->data.find( n );
+    if(it != std::end(this->data)){
+        (const_cast<cell<std::string>&>(*it)).val = val;
+    }else{
+        this->data.insert( n );
+    }
+    return;
+}
+
+void
+table2::remove(int64_t row, int64_t col){
+    cell<std::string> n(row, col, "");
+    this->data.erase(n);
+}
+
+void
+table2::visit_block( const std::pair<int64_t, int64_t>& row_bounds,
+                     const std::pair<int64_t, int64_t>& col_bounds,
+                     const visitor_func_t& f ){
+    if(!f){
+        throw std::invalid_argument("Invalid user functor");
+    }
+    for(int64_t row = row_bounds.first; row < row_bounds.second; ++row){
+        for(int64_t col = col_bounds.first; col < col_bounds.second; ++col){
+            cell<std::string> shtl(row, col, "");
+            cell<std::string>* cell_ptr = &shtl;
+            
+            auto it = this->data.find( shtl );
+            const bool cell_already_present = (it != std::end(this->data));
+            if(cell_already_present){
+                cell_ptr = &(const_cast<cell<std::string>&>(*it));
+            }
+
+            const auto res = f(row, col, cell_ptr->val);
+
+            if(cell_already_present){
+                if(false){
+                }else if(res == action::remove){
+                    this->data.erase(it);
+                }else if(res == action::automatic){
+                    if(cell_ptr->val.empty()){
+                        this->data.erase(it);
+                    }
+                }
+
+            }else{
+                if(false){
+                }else if(res == action::add){
+                    this->data.insert(*cell_ptr);
+                }else if(res == action::automatic){
+                    if(!(cell_ptr->val.empty())){
+                        this->data.insert(*cell_ptr);
+                    }
+                }
+            }
+        }
+    }
+    return;
+}
+
+void
+table2::visit_standard_block( const visitor_func_t& f ){
+    auto [min_row, max_row] = this->min_max_row();
+    auto [min_col, max_col] = this->min_max_col();
+    int64_t zero = 0;
+    int64_t five = 5;
+    this->visit_block( { std::min( zero, min_row ), std::max( five, max_row + 1 ) },
+                       { std::min( zero, min_col ), std::max( five, max_col + 1 ) },
+                       f );
+    return;
+}
+
+} // namespace tables
+
+
+/*
+
+// Sample usage
+
+int main(){
+
+    tables::table2 t;
+    t.data.emplace(12, 23, "test cell 1");
+    t.data.emplace(123, 234, "test cell 2");
+
+    const auto [min_row, max_row] = t.min_max_row();
+    const auto [min_col, max_col] = t.min_max_row();
+
+    std::cout << "Current min/max row: " << min_row << "/" << max_row << std::endl;
+    std::cout << "Current min/max col: " << min_col << "/" << max_col << std::endl;
+
+    std::cout << "Next empty row: " << t.next_empty_row() << std::endl;
+    std::cout << "Next empty col: " << t.next_empty_col() << std::endl;
+
+    std::cout << "Is (1, 2) present? " << !!t.value(1, 2) << std::endl;
+    std::cout << "Is (12, 23) present? " << !!t.value(12, 23) << std::endl;
+    std::cout << "Value of cell (12, 23): '" << t.value(12, 23).value().get() << "'" << std::endl;
+
+    std::cout << "Number of cells prior to visitation: " << t.data.size() << std::endl;
+
+    tables::visitor_func_t f_1 = [](int64_t row, int64_t col, std::string& v) -> tables::action {
+        if(!v.empty()){
+            std::cout << "Visiting non-empty cell " << row << ", " << col << " containing '" << v << "'" << std::endl;
+            v += " (visited)";
+        }
+        if( (row == 50) && (col == 75) ){
+            v += "(visitor added this cell)";
+        }
+        if( (row == 123) && (col == 234) ){
+            v.clear(); // This cell will now be removed via the automatic action.
+        }
+        return tables::action::automatic; // Retain only non-empty cells.
+    };
+
+    t.visit_standard_block(f_1);
+    std::cout << "Number of cells after visitation (automatic): " << t.data.size() << std::endl;
+
+    tables::visitor_func_t f_2 = [](int64_t row, int64_t col, std::string& v) -> tables::action {
+        return tables::action::add; // Add all cells, even if empty.
+    };
+
+    t.visit_standard_block(f_2);
+    std::cout << "Number of cells after visitation (add): " << t.data.size() << std::endl;
+
+
+    tables::visitor_func_t f_3 = [](int64_t row, int64_t col, std::string& v) -> tables::action {
+        return tables::action::remove; // Remove all cells, even if not empty.
+    };
+
+    t.visit_standard_block(f_3);
+    std::cout << "Number of cells after visitation (remove): " << t.data.size() << std::endl;
+
+    return 0;
+}
+*/
---
+++ src/Tables.h
@@ -0,0 +1,84 @@
+//Table.h -- a minimal 2D spreadsheet / 'stringly-typed' sparse matrix class.
+
+#pragma once
+
+#include <set>
+#include <map>
+#include <string>
+#include <optional>
+#include <functional>
+#include <utility>
+
+namespace tables {
+
+template<class T>
+class cell {
+    private:
+        int64_t row;
+        int64_t col;
+
+    public:
+        T val;
+
+        cell();
+        cell(int64_t r, int64_t c, const T& v);
+
+        int64_t get_row() const;
+        int64_t get_col() const;
+
+        bool operator==(const cell &) const;
+        bool operator!=(const cell &) const;
+        bool operator<(const cell &) const; 
+};
+
+// Controls how cells are treated during iteration.
+enum class action {
+    automatic, // automatically prune empty cells, add non-empty cells.
+    remove,
+    add,
+};
+
+using visitor_func_t = std::function< action (int64_t r, int64_t c, std::string& v)>;
+
+struct table2 {
+    //std::set< std::variant<cell<std::string>,
+    //                       cell<double>,
+    //                       cell<int64_t>>> data;
+    std::set< cell<std::string> > data;
+
+    std::map<std::string, std::string> metadata;
+
+    // Constructor.
+    table2();
+
+    // These functions return the (inclusive) bounds of the content currently in the table.
+    std::pair<int64_t, int64_t> min_max_row() const;
+    std::pair<int64_t, int64_t> min_max_col() const;
+
+    // These functions locate the next empty row or column to the bottom/right of all existing rows/columns.
+    // Note that holes in the interior are ignored. They are useful for appending data.
+    int64_t next_empty_row() const;
+    int64_t next_empty_col() const;
+
+    // Overwrite existing or insert new cell.
+    void inject(int64_t row, int64_t col, const std::string& val);
+
+    // Remove existing cell, if present.
+    void remove(int64_t row, int64_t col);
+
+    // Optional is disengaged if cell does not exist.
+    std::optional<std::reference_wrapper<std::string>> value(int64_t row, int64_t col);
+
+    // Visits every cell within the bounds (inclusive), even if not active.
+    // Whether the cell should be engaged or disengaged after iteration is controlled by the user functor.
+    void visit_block( const std::pair<int64_t, int64_t>& row_bounds,
+                      const std::pair<int64_t, int64_t>& col_bounds,
+                      const visitor_func_t& f );
+    
+    // Same as previous, but visits block from [r,c] = [min(0,min_row), min(0,min_col)] to [max(5,max_row+1), max(5,max_col+1)].
+    void visit_standard_block( const visitor_func_t& f );
+
+};
+
+} // namespace tables
+
