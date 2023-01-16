fa39fa7c2ea1c42e3d3822a85f1d0d2cbb93de05
Haley Clark <gmail.com@hdeanclark>
2023-01-16 13:56:00 -0800

    Add Transaction operation.
    This operation adds transaction semantics for operations. Child operations will either succeed, or be rolled back to a
    snapshot of the Drover object when the transaction started. Side-effects, of course, will not be transacted.

--- src/Operation_Dispatcher.cc
+++ src/Operation_Dispatcher.cc
@@ -196,6 +196,7 @@
 #include "Operations/ThresholdImages.h"
 #include "Operations/ThresholdOtsu.h"
 #include "Operations/Time.h"
+#include "Operations/Transaction.h"
 #include "Operations/TrimROIDose.h"
 #include "Operations/True.h"
 #include "Operations/UBC3TMRI_DCE.h"
@@ -436,6 +437,7 @@ std::map<std::string, op_packet_t> Known_Operations(){
     out["ThresholdImages"] = std::make_pair(OpArgDocThresholdImages, ThresholdImages);
     out["ThresholdOtsu"] = std::make_pair(OpArgDocThresholdOtsu, ThresholdOtsu);
     out["Time"] = std::make_pair(OpArgDocTime, Time);
+    out["Transaction"] = std::make_pair(OpArgDocTransaction, Transaction);
     out["TrimROIDose"] = std::make_pair(OpArgDocTrimROIDose, TrimROIDose);
     out["True"] = std::make_pair(OpArgDocTrue, True);
     out["UBC3TMRI_DCE_Differences"] = std::make_pair(OpArgDocUBC3TMRI_DCE_Differences, UBC3TMRI_DCE_Differences);
---
+++ src/Operations/Transaction.cc
@@ -0,0 +1,85 @@
+//Transaction.cc - A part of DICOMautomaton 2023. Written by hal clark.
+
+#include <any>
+#include <optional>
+#include <functional>
+#include <iterator>
+#include <list>
+#include <map>
+#include <memory>
+#include <regex>
+#include <stdexcept>
+#include <string>    
+
+#include "YgorImages.h"
+#include "YgorMath.h"         //Needed for vec3 class.
+#include "YgorString.h"       //Needed for GetFirstRegex(...)
+#include "YgorStats.h"
+
+#include "../Structs.h"
+#include "../Regex_Selectors.h"
+#include "../Operation_Dispatcher.h"
+
+#include "Transaction.h"
+
+
+
+OperationDoc OpArgDocTransaction(){
+    OperationDoc out;
+    out.name = "Transaction";
+
+    out.desc = 
+        "This operation will make a snapshot of the internal data state and execute children operations."
+        " If any child operation fails or returns false, the state will be reset to the snapshot and the remaining"
+        " children operations will not be executed."
+        " If all children operations succeed, any modifications to the modified state will be committed"
+        " automatically when the final operation succeeds, and the snapshot will be discarded.";
+
+    out.notes.emplace_back(
+        "This operation only transacts the internal state of the Drover object and the parameter table."
+        " Any side-effects caused by the children operations, such as modifying files, appending to logs, interaction"
+        " with terminals/consoles, or interacting over networks, will not be transacted."
+        " Side-effects will therefore be committed immediately, regardless of whether the transaction succeeds."
+    );
+    out.notes.emplace_back(
+        "This operation duplicates the full internal data state, so can be memory-intensive."
+    );
+
+    return out;
+}
+
+
+
+bool Transaction(Drover &DICOM_data,
+                 const OperationArgPkg& OptArgs,
+                 std::map<std::string, std::string>& InvocationMetadata,
+                 const std::string& FilenameLex){
+
+    //---------------------------------------------- User Parameters --------------------------------------------------
+    // ... nothing to load ...
+
+    //-----------------------------------------------------------------------------------------------------------------
+
+    auto children = OptArgs.getChildren();
+    if(children.empty()){
+        FUNCWARN("No children operations specified, forgoing transaction");
+    }else{
+
+        // Copy Drover and other relevant internal state.
+        const auto orig_DICOM_data = DICOM_data.Deep_Copy();
+        const auto orig_InvocationMetadata = InvocationMetadata;
+
+        // Perform children operations.
+        const bool res = Operation_Dispatcher(DICOM_data, InvocationMetadata, FilenameLex, children);
+        if(res){
+            FUNCINFO("Transaction succeeding. Committing state");
+            
+        }else{
+            FUNCWARN("Transaction failed. Reverting state");
+            DICOM_data = orig_DICOM_data;
+            InvocationMetadata = orig_InvocationMetadata;
+        }
+    }
+
+    return true;
+}
