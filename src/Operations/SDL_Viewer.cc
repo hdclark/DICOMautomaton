f4dde14f4ba07a02436d445d1cc7e53a58ff5133
Haley Clark <gmail.com@hdeanclark>
2023-02-06 13:02:46 -0800

    SDL_Viewer: layout tweak.

--- src/Operations/SDL_Viewer.cc
+++ src/Operations/SDL_Viewer.cc
@@ -3223,7 +3223,6 @@ bool SDL_Viewer(Drover &DICOM_data,
             ImGui::SameLine();
             const auto reset = ImGui::Button("Reset", ImVec2(window_extent.x/6, 0));
             
-            ImGui::SameLine();
             ImGui::Text("Current Score: %s, Current Speed: %s%%", std::to_string(static_cast<long int>(score)).c_str(),
                                                                   std::to_string(static_cast<long int>(100.0 * speed)).c_str());
 
