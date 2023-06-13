8a0617d7ea5f4918cff43d5191ed6d17a1ac1cc7
Haley Clark <gmail.com@hdeanclark>
2023-06-12 17:11:50 -0700

    Docker: add debian:bookworm dcma build image.

---
+++ docker/build_bases/debian_bookworm/make_phony_Debian_packages.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# This script creates phony/placeholder Debian packages. They are installed in the Docker image so that apt won't
+# overwrite a custom upstream package installation with the distribution version.
+
+# sudo apt-get install equivs
+# equivs-control libwt-dev.phony
+# equivs-control libwthttp-dev.phony
+#   <edit the files>
+equivs-build libwt-dev.phony 
+equivs-build libwthttp-dev.phony 
+
---
+++ docker/builders/debian_bookworm/implementation_debian_bookworm.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# This script installs DICOMautomaton. It assumes dependencies have been installed (i.e., the corresponding build_bases
+# script was completed successfully).
+
+set -eux
+
+# Install DICOMautomaton.
+
+# Option 1: install a binary package.
+#mkdir -pv /dcma
+#cd /dcma
+#apt-get install --yes -f ./DICOMautomaton*deb 
+
+# Option 2: clone the latest upstream commit.
+#mkdir -pv /dcma
+#cd /dcma
+#git clone https://github.com/hdclark/DICOMautomaton .
+#...
+
+# Option 3: assume a pristine repo is waiting at /dcma.
+cd /dcma
+./compile_and_install.sh -b build
+git reset --hard
+git clean -fxd :/ 
+
