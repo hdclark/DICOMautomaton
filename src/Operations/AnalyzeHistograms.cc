2d06e204038d543ebafb7d8e483d685255bd7af7
Hal Clark <gmail.com@hdeanclark>
2024-10-30 14:01:07 -0700

    AnalyzeHistograms: support storing results in parameter table.
    This commit allows dosimetric results to be stored in the global parameter table. Statements written this way have no
    inequality constraint, so they are not written to file. However, they are written to the global parameter table and so
    can be used by other operations.

--- src/Operations/AnalyzeHistograms.cc
+++ src/Operations/AnalyzeHistograms.cc
@@ -45,13 +45,18 @@ OperationDoc OpArgDocAnalyzeHistograms(){
 
     out.desc = 
         "This operation analyzes the selected line samples as if they were cumulative dose-volume histograms (DVHs)."
-        " Multiple criteria can be specified. The output is a CSV file that can be concatenated or appended to other"
-        " output files to provide a summary of multiple criteria.";
+        " Multiple criteria can be specified. Results can be written to a CSV file that can be concatenated or"
+        " appended to other output files to provide a summary of multiple criteria, or stored as key-values in the"
+        " global parameter table.";
 
     out.notes.emplace_back(
         "This routine will filter out non-matching line samples. Currently required: Modality=Histogram; each must be"
         " explicitly marked as a cumulative, unscaled abscissa + unscaled ordinate histogram; and differential"
         " distribution statistics must be available (e.g., min, mean, and max voxel doses)."
+    );
+    out.notes.emplace_back(
+        "No output files will be written when (1) there are no constraints to evaluate, and/or (2) results are written"
+        " in the global parameter table."
     );
 
     out.args.emplace_back();
@@ -64,7 +69,8 @@ OperationDoc OpArgDocAnalyzeHistograms(){
     out.args.back().name = "SummaryFilename";
     out.args.back().desc = "A summary of the criteria and results will be appended to this file."
                            " The format is CSV. Leave empty to dump to generate a unique temporary file."
-                           " If an existing file is present, rows will be appended without writing a header.";
+                           " If an existing file is present, rows will be appended without writing a header."
+                           " If there is nothing to write, a no file will be created.";
     out.args.back().default_val = "";
     out.args.back().expected = true;
     out.args.back().examples = { "", "/tmp/somefile", "localfile.csv", "derivative_data.csv" };
@@ -128,6 +134,11 @@ OperationDoc OpArgDocAnalyzeHistograms(){
                            " The RHS units can be any of {cc, cm3, cm^3, %} where"
                            " '%' means the inner LHS number is a percentage of the total volume."
                            "\n\n"
+                           "Additionally, dosimetric values can be assigned to a variable name"
+                           " and written to the global parameter store."
+                           " Dose is reported in the existing units, as-is (e.g., DICOM units; Gy)."
+                           " Note that variables assigned this way are not written to file."
+                           "\n\n"
                            "Multiple constraints can be supplied by separating them with ';' delimiters."
                            " Each will be evaluated separately."
                            " Newlines can also be used, though constraints should all end with a ';'."
@@ -147,6 +158,8 @@ OperationDoc OpArgDocAnalyzeHistograms(){
                                  "V(24.5 Gy) < 500.0 cc",
                                  "V(10%) < 50.0 cc",
                                  "V(24.5 Gy) < 500.0 cc",
+                                 "max_dose = Dmax",
+                                 "x = D(coldest 10%)",
                                  "Dmax < 50.0 Gy ; Dmean lte 80 % ; D(hottest 10%) gte 95.0 %" };
 
 
@@ -187,9 +200,9 @@ lCompile_Regex(const std::string& input){
                              std::regex::extended);
 }
 bool AnalyzeHistograms(Drover &DICOM_data,
-                         const OperationArgPkg& OptArgs,
-                         std::map<std::string, std::string>& /*InvocationMetadata*/,
-                         const std::string& /*FilenameLex*/){
+                       const OperationArgPkg& OptArgs,
+                       std::map<std::string, std::string>& InvocationMetadata,
+                       const std::string& /*FilenameLex*/){
 
     //---------------------------------------------- User Parameters --------------------------------------------------
     const auto LineSelectionStr = OptArgs.getValueStr("LineSelection").value();
@@ -292,24 +305,28 @@ bool AnalyzeHistograms(Drover &DICOM_data,
                 return;
             };
 
-            // Generalize the inequality.
-            const auto r_lt  = lCompile_Regex(R"***(^.*[<][^=].*$|^.*lt[^e].*$)***");
-            const auto r_gt  = lCompile_Regex(R"***(^.*[>][^=].*$|^.*gt[^e].*$)***");
-            const auto r_lte = lCompile_Regex(R"***(^.*[<][=].*$|^.*lte.*$)***");
-            const auto r_gte = lCompile_Regex(R"***(^.*[>][=].*$|^.*gte.*$)***");
+            // Classify the statement.
+            const auto r_lt     = lCompile_Regex(R"***(^.*[<][^=].*$|^.*lt[^e].*$)***");
+            const auto r_gt     = lCompile_Regex(R"***(^.*[>][^=].*$|^.*gt[^e].*$)***");
+            const auto r_lte    = lCompile_Regex(R"***(^.*[<][=].*$|^.*lte.*$)***");
+            const auto r_gte    = lCompile_Regex(R"***(^.*[>][=].*$|^.*gte.*$)***");
+            const auto r_assign = lCompile_Regex(R"***(^.*[^><][=].*$|^.*as.*$)***");
 
-            enum class equality_t   { na, lt, lte, gt, gte };   // inequality type.
+            enum class equality_t { na, assign, lt, lte, gt, gte };   // inequality type.
             equality_t eq = equality_t::na;
 
-            if( std::regex_match(ac, r_lt ) ){    eq = equality_t::lt;
-            }else if( std::regex_match(ac, r_gt ) ){    eq = equality_t::gt;
-            }else if( std::regex_match(ac, r_lte) ){    eq = equality_t::lte;
-            }else if( std::regex_match(ac, r_gte) ){    eq = equality_t::gte;
+            if( false ){
+            }else if( std::regex_match(ac, r_lt     ) ){    eq = equality_t::lt;
+            }else if( std::regex_match(ac, r_gt     ) ){    eq = equality_t::gt;
+            }else if( std::regex_match(ac, r_lte    ) ){    eq = equality_t::lte;
+            }else if( std::regex_match(ac, r_gte    ) ){    eq = equality_t::gte;
+            }else if( std::regex_match(ac, r_assign ) ){    eq = equality_t::assign;
             }else{ throw std::invalid_argument("No inequality type recognized. Cannot continue.");
             }
 
             const auto compare_inequality = [&](double L, double R) -> bool {
-                if(eq == equality_t::lt ){   return (L < R);
+                if( false ){
+                }else if(eq == equality_t::lt ){   return (L < R);
                 }else if(eq == equality_t::gt ){   return (L > R);
                 }else if(eq == equality_t::lte){   return (L <= R);
                 }else if(eq == equality_t::gte){   return (L >= R);
@@ -317,6 +334,18 @@ bool AnalyzeHistograms(Drover &DICOM_data,
                 throw std::logic_error("Unrecognized inequality type. Cannot continue.");
                 return false; // Should never get here.
             };
+
+            const auto r_min  = lCompile_Regex(R"***(.*min.*)***");
+            const auto r_mean = lCompile_Regex(R"***(.*mean.*)***");
+            const auto r_max  = lCompile_Regex(R"***(.*max.*)***");
+
+            const auto r_gy   = lCompile_Regex(R"***(.*Gy.*)***");
+            const auto r_pcnt = lCompile_Regex(R"***(.*[%].*)***");
+
+            const auto r_cc   = lCompile_Regex(R"***(.*cc.*|.*cm3.*|.*cm\^3.*)***");
+
+            const auto r_hot  = lCompile_Regex(R"***(.*hot.*)***");
+            const auto r_cold = lCompile_Regex(R"***(.*cold.*)***");
 
             /////////////////////////////////////////////////////////////////////////////////
             // D{min,mean,max} {<,<=,>=,>,lt,lte,gt,gte} 123.123 {%,Gy}.
@@ -333,16 +362,10 @@ bool AnalyzeHistograms(Drover &DICOM_data,
                 const auto D_rhs = std::stod(p.at(2));
                 const auto unit = p.at(3);
 
-                const auto r_min  = lCompile_Regex(R"***(.*min.*)***");
-                const auto r_mean = lCompile_Regex(R"***(.*mean.*)***");
-                const auto r_max  = lCompile_Regex(R"***(.*max.*)***");
-
-                const auto r_gy   = lCompile_Regex(R"***(.*Gy.*)***");
-                const auto r_pcnt = lCompile_Regex(R"***(.*[%].*)***");
-
                 // Determine which statistic is needed.
                 double D_mmm = nan;
-                if( std::regex_match(mmm, r_min ) ){    D_mmm = MinDose;
+                if( false ){
+                }else if( std::regex_match(mmm, r_min ) ){    D_mmm = MinDose;
                 }else if( std::regex_match(mmm, r_mean) ){    D_mmm = MeanDose;
                 }else if( std::regex_match(mmm, r_max ) ){    D_mmm = MaxDose;
                 }else{  throw std::runtime_error("Unable to parse constraint (D).");
@@ -351,7 +374,8 @@ bool AnalyzeHistograms(Drover &DICOM_data,
                 // Scale the LHS statistic to match the units of the RHS so we can compare them and report the actual
                 // LHS value (which makes it easier for the user to compare).
                 std::string out_unit;
-                if( std::regex_match(unit, r_gy ) ){
+                if( false ){
+                }else if( std::regex_match(unit, r_gy ) ){
                     D_mmm *= 1.0; // a no-op.
                     out_unit = "Gy";
                 }else if( std::regex_match(unit, r_pcnt ) ){
@@ -390,13 +414,6 @@ bool AnalyzeHistograms(Drover &DICOM_data,
                 const auto ineq = p.at(3); // < or <= or ... .
                 const auto D_rhs = std::stod(p.at(4)); // dose number.
                 const auto RHS_unit = p.at(5); // Gy or %.
-
-                const auto r_cc   = lCompile_Regex(R"***(.*cc.*|.*cm3.*|.*cm\^3.*)***");
-                const auto r_pcnt = lCompile_Regex(R"***(.*[%].*)***");
-                const auto r_gy   = lCompile_Regex(R"***(.*Gy.*)***");
-
-                const auto r_hot  = lCompile_Regex(R"***(.*hot.*)***");
-                const auto r_cold = lCompile_Regex(R"***(.*cold.*)***");
                 
                 // Determine the equivalent absolute volume from the inner LHS.
                 double V_abs = nan; // in mm^3.
@@ -445,7 +462,8 @@ bool AnalyzeHistograms(Drover &DICOM_data,
 
                 // Express the LHS evaluated dose in the same units that the RHS has been stated.
                 std::string out_unit;
-                if( std::regex_match(RHS_unit, r_gy ) ){
+                if( false ){
+                }else if( std::regex_match(RHS_unit, r_gy ) ){
                     D_eval *= 1.0; // A no-op.
                     out_unit = "Gy";
                 }else if( std::regex_match(RHS_unit, r_pcnt ) ){
@@ -487,13 +505,10 @@ bool AnalyzeHistograms(Drover &DICOM_data,
 
                 const auto V_total = DVH_abs_D_abs_V.Get_Extreme_Datum_y().second[2];
 
-                const auto r_cc   = lCompile_Regex(R"***(.*cc.*|.*cm3.*|.*cm\^3.*)***");
-                const auto r_pcnt = lCompile_Regex(R"***(.*[%].*)***");
-                const auto r_gy   = lCompile_Regex(R"***(.*Gy.*)***");
-                
                 // Determine the equivalent absolute dose from the inner LHS.
                 double D_abs = nan; // in Gy.
-                if( std::regex_match(LHS_unit, r_gy ) ){
+                if( false ){
+                }else if( std::regex_match(LHS_unit, r_gy ) ){
                     D_abs = D_lhs; // A no-op.
                 }else if( std::regex_match(LHS_unit, r_pcnt) ){
                     D_abs = D_lhs * ReferenceDose / 100.0; // Convert from % to abs.
@@ -512,7 +527,8 @@ bool AnalyzeHistograms(Drover &DICOM_data,
 
                 // Express the LHS evaluated volume in the same units that the RHS has been stated.
                 std::string out_unit;
-                if( std::regex_match(RHS_unit, r_cc ) ){
+                if( false ){
+                }else if( std::regex_match(RHS_unit, r_cc ) ){
                     // Convert mm^3 to cm^3.
                     V_eval /= 1000.0;
                     out_unit = "cm^3";
@@ -535,6 +551,95 @@ bool AnalyzeHistograms(Drover &DICOM_data,
 
                 header << std::endl;
                 report << std::endl;
+
+            /////////////////////////////////////////////////////////////////////////////////
+            // Assignment:
+            //   var_name = D{min,mean,max}
+            }else if(auto q = lCompile_Regex(R"***([ ]*([^ =]*)[ ]*[=][ ]*D(min|max|mean).*)***");
+                     std::regex_match(ac, q) ){
+
+                const auto p = Get_All_Regex(ac, q);
+                if(p.size() != 2) throw std::runtime_error("Unable to parse constraint (F).");
+                const auto var_name = p.at(0);
+                const auto mmm = p.at(1);
+
+                InvocationMetadata.erase(var_name);
+
+                double D_mmm = nan;
+                if( false ){
+                }else if( std::regex_match(mmm, r_min ) ){    D_mmm = MinDose;
+                }else if( std::regex_match(mmm, r_mean) ){    D_mmm = MeanDose;
+                }else if( std::regex_match(mmm, r_max ) ){    D_mmm = MaxDose;
+                }else{  throw std::runtime_error("Unable to parse constraint (F).");
+                }
+
+                InvocationMetadata[var_name] = std::to_string(D_mmm);
+
+            /////////////////////////////////////////////////////////////////////////////////
+            // Assignment:
+            //   var_name = D( hottest 500 cc)
+            //   var_name = D( hottest 500 cc) 
+            //   var_name = D( coldest 25% )
+            }else if(auto q = lCompile_Regex(R"***([ ]*([^ =]*)[ ]*[=][ ]*D[(][ ]*(hott?e?s?t?|cold?e?s?t?)[ ]*([0-9.]+)[ ]*(cc|cm3|cm\^3|%)[ ]*[)][ ]*)***");
+                     std::regex_match(ac, q) ){
+
+                const auto p = Get_All_Regex(ac, q);
+                if(p.size() != 4) throw std::runtime_error("Unable to parse constraint (G).");
+                //for(const auto &x : p) YLOGINFO("    Parsed parameter: '" << x << "'");                
+
+                const auto var_name = p.at(0);
+                const auto HC = p.at(1); // hot or cold.
+                const auto V_rhs = std::stod(p.at(2)); // inner volume number.
+                const auto V_unit = p.at(3); // cc or cm3 or cm^3 or %.
+
+                InvocationMetadata.erase(var_name);
+                
+                // Determine the equivalent absolute volume from the inner RHS.
+                double V_abs = nan; // in mm^3.
+                if( std::regex_match(V_unit, r_cc ) ){
+                    // Convert cm^3 to mm^3.
+                    V_abs = V_rhs * 1000.0;
+                }else if( std::regex_match(V_unit, r_pcnt) ){
+                    // Convert from % to mm^3 by taking the highest bin (which should contain all voxels) scaling the entire volume.
+                    const auto V_total = DVH_abs_D_abs_V.Get_Extreme_Datum_y().second[2];
+                    V_abs = V_total * V_rhs / 100.0;
+                }else{ 
+                    throw std::runtime_error("Unable to convert inner RHS V units.");
+                }
+
+                // Find the corresponding D from the RHS.
+                double D_eval = nan;
+                if( std::regex_match(HC, r_hot ) ){
+                    // For a cumulative DVH, we merely have to find the right-most crossing point of the volume
+                    // threshold and the DVH curve.
+                    const auto crossings = DVH_abs_D_abs_V.Crossings(V_abs);
+                    if(crossings.empty()){
+                        throw std::invalid_argument("RHS inner constraint numeric volumetric value cannot be evaluated. Cannot continue.");
+                    }
+                    D_eval = crossings.Get_Extreme_Datum_x().second[0];
+
+                }else if( std::regex_match(HC, r_cold ) ){
+                    // For a cumulative DVH, we merely have to find the left-most crossing point of the volume
+                    // threshold and the DVH curve. The volume threshold is modified by subtracting from the maximum
+                    // since the left-most accumulates all voxels, regardless of dose (so the left-most bin should
+                    // contain all voxels). 
+                    const auto V_total = DVH_abs_D_abs_V.Get_Extreme_Datum_y().second[2];
+                    const auto V_intersect = V_total - V_abs;
+                    if(!isininc(0.0, V_intersect, V_total)){
+                        throw std::invalid_argument("Volumetric constraint cannot be evaluated; insufficient target volume.");
+                    }
+
+                    const auto crossings = DVH_abs_D_abs_V.Crossings(V_intersect);
+                    if(crossings.empty()){
+                        throw std::invalid_argument("RHS inner constraint numeric volumetric value cannot be evaluated. Cannot continue.");
+                    }
+                    D_eval = crossings.Get_Extreme_Datum_x().first[0];
+
+                }else{
+                    throw std::runtime_error("Unable to parse directionality (i.e., hot or cold).");
+                }
+
+                InvocationMetadata[var_name] = std::to_string(D_eval);
 
             // If the constraint did not match any known format.
             }else{
@@ -561,7 +666,9 @@ bool AnalyzeHistograms(Drover &DICOM_data,
     //YLOGINFO("Report will contain: " << report.str());
 
     //Write the report to file.
-    if(!LSs.empty()){
+    if( !LSs.empty()
+    &&  !header.str().empty()
+    &&  !report.str().empty() ){
         auto gen_filename = [&]() -> std::string {
             if(!SummaryFilename.empty()){
                 return SummaryFilename;
