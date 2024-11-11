5fca29aa9ba54f20665f36b0205b07f211903886
Haley Clark <gmail.com@hdeanclark>
2024-11-10 21:48:09 -0800

    Add For operation.
    This operation adds both counter-based and discrete-based looping.

---
+++ src/Operations/For.h
@@ -0,0 +1,16 @@
+// For.h.
+
+#pragma once
+
+#include <map>
+#include <string>
+
+#include "../Structs.h"
+
+
+OperationDoc OpArgDocFor();
+
+bool For(Drover &DICOM_data,
+         const OperationArgPkg& /*OptArgs*/,
+         std::map<std::string, std::string>& /*InvocationMetadata*/,
+         const std::string& /*FilenameLex*/);
