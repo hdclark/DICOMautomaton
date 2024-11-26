45163a48d1f9d58acf5cdf0494410c9c4fa971bc
Hal Clark <gmail.com@hdeanclark>
2024-11-26 09:46:15 -0800

    ConvertParametersToTable: add support for both 'wide' and 'tall' shapes.

--- src/Operations/ConvertParametersToTable.cc
+++ src/Operations/ConvertParametersToTable.cc
@@ -43,15 +43,15 @@ OperationDoc OpArgDocConvertParametersToTable(){
     out.desc = 
         "Convert one or more key-value parameters from the global parameter table into a table."
         " If no table is selected, a new table will be created."
-        " If an existing table is selected, it will be appended to an empty row at the bottom of the table.";
+        " If an existing table is selected, row(s) will be appended to the bottom.";
 
     out.args.emplace_back();
     out.args.back().name = "KeySelection";
     out.args.back().desc = "A regular expression that will select key-values to include in the table."
                            "\n\n"
                            "This regular expression will be applied only to keys."
-                           " Note that multiple keys can be selected, and all matches will be printed on"
-                           " the same row."
+                           " Note that multiple keys can be selected; whether they are emitted on one"
+                           " or multiple rows is controlled by the 'Shape' parameter."
                            "";
     out.args.back().default_val = ".*";
     out.args.back().expected = true;
@@ -74,18 +74,36 @@ OperationDoc OpArgDocConvertParametersToTable(){
 
     out.args.emplace_back();
     out.args.back().name = "EmitHeader";
-    out.args.back().desc = "Controls whether a header is emitted."
+    out.args.back().desc = "Controls whether a header (consisting of the key names) is emitted."
                            " If 'false' no header is emitted."
                            " If 'true', two rows are emitted regardless of whether there is a pre-existing header."
                            " If 'empty', a header is only emitted when the table is empty (i.e., no content in any"
                            " cells). Consistency of the emitted row with the existing table content and"
                            " structure is not verified and is therefore left to the user."
-                           "\n\n"
-                           "Note that the header consists of the key names."
                            "";
     out.args.back().default_val = "empty";
     out.args.back().expected = true;
     out.args.back().examples = { "true", "false", "empty" };
+    out.args.back().samples = OpArgSamples::Exhaustive;
+
+
+    out.args.emplace_back();
+    out.args.back().name = "Shape";
+    out.args.back().desc = "Controls whether the table is written in 'wide' or 'tall' format."
+                           "\n\n"
+                           "The 'wide' shape places all output from a single invocation onto a single row"
+                           " (or two if a header is also emitted)."
+                           " This format is most useful for analysis since the relationship between metadata"
+                           " elements can be analyzed more easily (e.g., regression or classification)."
+                           "\n\n"
+                           "The 'tall' shape places every metadata key-value pair on a separate row."
+                           " This format is most useful for simplistic data extraction or simple analysis where"
+                           " the relationship between metadata elements is not important (e.g., grepping for a"
+                           " specific key-value, checking if one-of-any tags are present)."
+                           "";
+    out.args.back().default_val = "wide";
+    out.args.back().expected = true;
+    out.args.back().examples = { "wide", "tall" };
     out.args.back().samples = OpArgSamples::Exhaustive;
 
     return out;
@@ -104,6 +122,7 @@ bool ConvertParametersToTable(Drover& DICOM_data,
     const auto TableLabel = OptArgs.getValueStr("TableLabel").value();
     const auto TableSelectionStr = OptArgs.getValueStr("TableSelection").value();
     const auto EmitHeaderStr = OptArgs.getValueStr("EmitHeader").value();
+    const auto ShapeStr = OptArgs.getValueStr("Shape").value();
 
     //-----------------------------------------------------------------------------------------------------------------
     const auto NormalizedTableLabel = X(TableLabel);
@@ -113,10 +132,16 @@ bool ConvertParametersToTable(Drover& DICOM_data,
     const auto regex_true = Compile_Regex("^tr?u?e?$");
     const auto regex_false = Compile_Regex("^fa?l?s?e?$");
     const auto regex_empty = Compile_Regex("^em?p?t?y?$");
+
+    const auto regex_wide = Compile_Regex("^wi?d?e?$");
+    const auto regex_tall = Compile_Regex("^ta?l?l?$");
     
     const bool emit_header_true  = std::regex_match(EmitHeaderStr, regex_true);
     const bool emit_header_false = std::regex_match(EmitHeaderStr, regex_false);
     const bool emit_header_empty = std::regex_match(EmitHeaderStr, regex_empty);
+
+    const bool shape_wide = std::regex_match(ShapeStr, regex_wide);
+    const bool shape_tall = std::regex_match(ShapeStr, regex_tall);
 
     // Select or create a table.
     auto STs_all = All_STs( DICOM_data );
@@ -128,35 +153,68 @@ bool ConvertParametersToTable(Drover& DICOM_data,
 
 
     // Search all parameters.
-    const int64_t row_a = st->table.next_empty_row();
-    const int64_t row_b = row_a + 1;
+    int64_t row_a = st->table.next_empty_row();
+    int64_t row_b = row_a + 1;
     tables::table2 placeholder_table;
     const bool is_empty = create_new_table || (row_a == placeholder_table.next_empty_row());
 
-    int64_t col = 1;
+    int64_t col = 0;
+    int64_t row = row_a;
     for(const auto& kvp : InvocationMetadata){
         if(std::regex_match(kvp.first, regex_key)){
-            std::optional<int64_t> row_hdr;
-            std::optional<int64_t> row_val;
 
             if(false){
-            }else if( emit_header_true
-                  ||  (emit_header_empty && is_empty) ){
-                row_hdr = row_a;
-                row_val = row_b;
-            }else if( emit_header_false
-                  ||  (emit_header_empty && !is_empty) ){
-                row_hdr = {};
-                row_val = row_a;
-            }else{
-                throw std::runtime_error("EmitHeader argument not understood");
-            }
+            }else if(shape_wide){
+                std::optional<int64_t> row_hdr;
+                std::optional<int64_t> row_val;
+                if(false){
+                }else if( emit_header_true
+                      ||  (emit_header_empty && is_empty) ){
+                    row_hdr = row_a;
+                    row_val = row_b;
+                }else if( emit_header_false
+                      ||  (emit_header_empty && !is_empty) ){
+                    row_hdr = {};
+                    row_val = row_a;
+                }else{
+                    throw std::runtime_error("EmitHeader argument not understood");
+                }
 
-            if(row_hdr){
-                st->table.inject(row_hdr.value(), col, kvp.first);
-            }
-            if(row_val){
-                st->table.inject(row_val.value(), col++, kvp.second);
+                if(row_hdr){
+                    st->table.inject(row_hdr.value(), col, kvp.first);
+                }
+                if(row_val){
+                    st->table.inject(row_val.value(), col, kvp.second);
+                }
+                ++col;
+
+            }else if(shape_tall){
+                std::optional<int64_t> col_hdr;
+                std::optional<int64_t> col_val;
+
+                if(false){
+                }else if( emit_header_true
+                      ||  (emit_header_empty && is_empty) ){
+                    col_hdr = 0;
+                    col_val = 1;
+                }else if( emit_header_false
+                      ||  (emit_header_empty && !is_empty) ){
+                    col_hdr = {};
+                    col_val = 0;
+                }else{
+                    throw std::runtime_error("EmitHeader argument not understood");
+                }
+
+                if(col_hdr){
+                    st->table.inject(row, col_hdr.value(), kvp.first);
+                }
+                if(col_val){
+                    st->table.inject(row, col_val.value(), kvp.second);
+                }
+                ++row;
+
+            }else{
+                throw std::runtime_error("Shape argument not understood");
             }
         }
     }
