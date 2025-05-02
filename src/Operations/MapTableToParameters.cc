247bd9aebd739177877f74dd35b21f24414dbfcf
Hal Clark <gmail.com@hdeanclark>
2025-05-02 14:15:15 -0700

    MapTableToParameters: add alias (ConvertTableToParameters).

--- src/Operations/MapTableToParameters.cc
+++ src/Operations/MapTableToParameters.cc
@@ -40,6 +40,7 @@
 OperationDoc OpArgDocMapTableToParameters(){
     OperationDoc out;
     out.name = "MapTableToParameters";
+    out.aliases.emplace_back("ConvertParametersToTable");
 
     out.tags.emplace_back("category: parameter table");
     out.tags.emplace_back("category: table processing");
