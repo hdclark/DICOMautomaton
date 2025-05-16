a2dd15c78ccf85ce744e32c01e4ce0acfad45508
Hal Clark <gmail.com@hdeanclark>
2025-05-16 11:47:17 -0700

    ModifyLineSamples: add additional methods from libygor.

--- src/Operations/ModifyLineSamples.cc
+++ src/Operations/ModifyLineSamples.cc
@@ -96,15 +96,62 @@ OperationDoc OpArgDocModifyLineSamples(){
                            " value."
                            "\n\n"
                            "Option 'purge-nonfinite' censors all datum with infinite or NaN coordinates."
-//                           "\n\n"
-//                           "Option '' "
-//                           ""
-//                           "\n\n"
-//                           "Option '' "
-//                           ""
-//                           "\n\n"
-//                           "Option '' "
-                           ""
+                           "\n\n"
+                           "Option 'histogram' generates a histogram with N equal-width bins. Each bin's height is"
+                           " the sum of the samples that appear within the bin's domain. This method can also optionally"
+                           " add an outline surrounding the histogram bins for visualization purposes by supplying a"
+                           " second numerical argument that evaluates to 'true' (e.g., 1)."
+                           "\n\n"
+                           "Option 'moving-average-two-sided-15-sample' computes the \"Spencer's\" 15-sample moving average,"
+                           " averaging the ordinates. This is a convolution that effectively acts like a low-pass"
+                           " filter, letting polynomials of order 3 or less through approximately as-is."
+                           "\n\n"
+                           "Option 'moving-average-two-sided-23-sample' computes the \"Henderson's\" 23-sample moving average,"
+                           " averaging the ordinates. This is a convolution that effectively acts like a low-pass"
+                           " filter, letting polynomials of order 3 or less through approximately as-is."
+                           "\n\n"
+                           "Option 'moving-average-two-sided-equal-weighting' computes a $(2N+1)$-sample moving average,"
+                           " averaging the ordinates with equal weighting. The discrete window size $N$ must be supplied."
+                           "\n\n"
+                           "Option 'moving-average-two-sided-gaussian-weighting' computes a moving average,"
+                           " averaging the ordinates using gaussian weighting. The width of the gaussian ($\\sigma$)"
+                           " must be supplied. Applying consecutively emulates applying once with a larger width."
+                           "\n\n"
+                           "Option 'moving-median-filter-two-sided-equal-weighting' computes a $(2N+1)$-sample moving"
+                           " median filter of the ordinate values. All ordinates are weighted identically."
+                           " The discrete window size $N$ must be supplied."
+                           "\n\n"
+                           "Option 'moving-median-filter-two-sided-gaussian-weighting' computes a $(2N+1)$-sample moving"
+                           " median filter of the ordinate values, using gaussian weighting to scale ordinates based on"
+                           " their distance. The width of the gaussian ($\\sigma$) must be supplied."
+                           "\n\n"
+                           "Option 'moving-median-filter-two-sided-triangular-weighting' computes a $(2N+1)$-sample moving"
+                           " median filter of the ordinate values. All ordinates are weighted linearly based on their"
+                           " rank position relative to the averaging point."
+                           " The discrete window size $N$ must be supplied."
+                           "\n\n"
+                           "Option 'moving-variance-two-sided' calculates an unbiased estimate of a population's"
+                           " variance over a window of ($2N+1$) samples. Endpoint variance estimation uses fewer"
+                           " samples (min = N) and have higher variance. Setting N to be 5 or higher is recommended."
+                           " The discrete window size $N$ must be supplied."
+                           "\n\n"
+                           "Option 'derivative-forward-finite-differences' calculates the discrete derivative using"
+                           " forward finite differences. The right-side endpoint uses backward finite differences to"
+                           " handle the boundary. Data should be reasonably smooth -- no interpolation is used."
+                           "\n\n"
+                           "Option 'derivative-backward-finite-differences' calculates the discrete derivative using"
+                           " backward finite differences. The left-side endpoint uses forward finite differences to"
+                           " handle the boundary. Data should be reasonably smooth -- no interpolation is used."
+                           "\n\n"
+                           "Option 'derivative-backward-centered-differences' calculates the discrete derivative using"
+                           " centered finite differences. The endpoints use either forward or backward finite"
+                           " differences to handle the boundaries. Data should be reasonably smooth -- no"
+                           " interpolation is used."
+                           "\n\n"
+                           "Option 'local-signed-curvature-three-sample' calculates the local signed curvature at"
+                           " each sample using the two nearest-neighbour samples."
+                           " Endpoints are discarded. Curvature here is the tangent circle's inverse radius, and the"
+                           " sign indicates the direction of concavity."
                            "";
     out.args.back().default_val = "";
     out.args.back().expected = true;
@@ -122,9 +169,21 @@ OperationDoc OpArgDocModifyLineSamples(){
                                  "multiply-scalar(1.25)",
                                  "sum-scalar(-1.23)",
                                  "absolute-ordinate()",
-                                 "purge-nonfinite",
-//                                 "",
-//                                 "",
+                                 "purge-nonfinite()",
+                                 "histogram(100)",
+                                 "histogram(100, 1)",
+                                 "moving-average-two-sided-15-sample()",
+                                 "moving-average-two-sided-23-sample()",
+                                 "moving-average-two-sided-equal-weighting(5)",
+                                 "moving-average-two-sided-gaussian-weighting(1.23)",
+                                 "moving-median-filter-two-sided-equal-weighting(5)",
+                                 "moving-median-filter-two-sided-gaussian-weighting(1.23)",
+                                 "moving-median-filter-two-sided-triangular-weighting(5)",
+                                 "moving-variance-two-sided(5)",
+                                 "derivative-forward-finite-differences()",
+                                 "derivative-backward-finite-differences()",
+                                 "derivative-centered-finite-differences()",
+                                 "local-signed-curvature-three-sample()",
                                  };
 
     return out;
@@ -156,13 +215,19 @@ bool ModifyLineSamples(Drover &DICOM_data,
     const auto method_sums     = rg.insert("sum-scalar");
     const auto method_abs      = rg.insert("absolute-ordinate");
     const auto method_pnf      = rg.insert("purge-nonfinite");
-//    const auto method_         = rg.insert("");
-//    const auto method_         = rg.insert("");
-//    const auto method_         = rg.insert("");
-//    const auto method_         = rg.insert("");
-//    const auto method_         = rg.insert("");
-//    const auto method_         = rg.insert("");
-//    const auto method_         = rg.insert("");
+    const auto method_hist     = rg.insert("histogram");
+    const auto method_ma15     = rg.insert("moving-average-two-sided-15-sample");
+    const auto method_ma23     = rg.insert("moving-average-two-sided-23-sample");
+    const auto method_maew     = rg.insert("moving-average-two-sided-equal-weighting");
+    const auto method_magw     = rg.insert("moving-average-two-sided-gaussian-weighting");
+    const auto method_mmfew    = rg.insert("moving-median-filter-two-sided-equal-weighting");
+    const auto method_mmfgw    = rg.insert("moving-median-filter-two-sided-gaussian-weighting");
+    const auto method_mmftw    = rg.insert("moving-median-filter-two-sided-triangular-weighting");
+    const auto method_mv       = rg.insert("moving-variance-two-sided");
+    const auto method_dffd     = rg.insert("derivative-forward-finite-differences");
+    const auto method_dbfd     = rg.insert("derivative-backward-finite-differences");
+    const auto method_dcfd     = rg.insert("derivative-centered-finite-differences");
+    const auto method_lsc3s    = rg.insert("local-signed-curvature-three-sample");
 
 /*
 struct function_parameter {
@@ -194,7 +259,7 @@ struct parsed_function {
     YLOGINFO("Selected " << LSs.size() << " line samples");
 
     for(const auto &pf : pfs){
-        YLOGDEBUG("Attempting method '" << pf.name << "' now");
+        YLOGINFO("Attempting method '" << pf.name << "' now");
         if(!pf.children.empty()){
             throw std::invalid_argument("Children functions are not accepted");
 
@@ -366,6 +431,149 @@ struct parsed_function {
             }
             for(auto & lsp_it : LSs){
                 (*lsp_it)->line = (*lsp_it)->line.Purge_Nonfinite_Samples();
+            }
+
+        // histogram
+        }else if(rg.matches(pf.name, method_hist)){
+            if( (pf.parameters.size() != 1UL)
+            &&  (pf.parameters.size() != 2UL) ){
+                throw std::invalid_argument("Incorrect number of arguments");
+            }
+            const auto n = pf.parameters.at(0).number.value();
+            const auto N = static_cast<int64_t>(n);
+
+            bool explicit_bins = false;
+            if(pf.parameters.size() == 2UL){
+                const auto b = pf.parameters.at(1).number.value();
+                explicit_bins = static_cast<bool>(b);
+            }
+
+            for(auto & lsp_it : LSs){
+                (*lsp_it)->line = (*lsp_it)->line.Histogram_Equal_Sized_Bins(N, explicit_bins);
+            }
+
+        // moving-average-two-sided-15-sample
+        }else if(rg.matches(pf.name, method_ma15)){
+            if(pf.parameters.size() != 0UL){
+                throw std::invalid_argument("Incorrect number of arguments");
+            }
+            for(auto & lsp_it : LSs){
+                (*lsp_it)->line = (*lsp_it)->line.Moving_Average_Two_Sided_Spencers_15_point();
+            }
+
+        // moving-average-two-sided-23-sample
+        }else if(rg.matches(pf.name, method_ma23)){
+            if(pf.parameters.size() != 0UL){
+                throw std::invalid_argument("Incorrect number of arguments");
+            }
+            for(auto & lsp_it : LSs){
+                (*lsp_it)->line = (*lsp_it)->line.Moving_Average_Two_Sided_Hendersons_23_point();
+            }
+
+        // moving-average-two-sided-equal-weighting
+        }else if(rg.matches(pf.name, method_maew)){
+            if(pf.parameters.size() != 1UL){
+                throw std::invalid_argument("Incorrect number of arguments");
+            }
+            const auto n = pf.parameters.at(0).number.value();
+            const auto N = static_cast<int64_t>(n);
+
+            for(auto & lsp_it : LSs){
+                (*lsp_it)->line = (*lsp_it)->line.Moving_Average_Two_Sided_Equal_Weighting(N);
+            }
+
+        // moving-average-two-sided-gaussian-weighting
+        }else if(rg.matches(pf.name, method_magw)){
+            if(pf.parameters.size() != 1UL){
+                throw std::invalid_argument("Incorrect number of arguments");
+            }
+            const auto sigma = pf.parameters.at(0).number.value();
+
+            for(auto & lsp_it : LSs){
+                (*lsp_it)->line = (*lsp_it)->line.Moving_Average_Two_Sided_Gaussian_Weighting(sigma);
+            }
+
+        // moving-median-filter-two-sided-equal-weighting
+        }else if(rg.matches(pf.name, method_mmfew)){
+            if(pf.parameters.size() != 1UL){
+                throw std::invalid_argument("Incorrect number of arguments");
+            }
+            const auto n = pf.parameters.at(0).number.value();
+            const auto N = static_cast<int64_t>(n);
+
+            for(auto & lsp_it : LSs){
+                (*lsp_it)->line = (*lsp_it)->line.Moving_Median_Filter_Two_Sided_Equal_Weighting(N);
+            }
+
+        // moving-median-filter-two-sided-gaussian-weighting
+        }else if(rg.matches(pf.name, method_mmfgw)){
+            if(pf.parameters.size() != 1UL){
+                throw std::invalid_argument("Incorrect number of arguments");
+            }
+            const auto sigma = pf.parameters.at(0).number.value();
+
+            for(auto & lsp_it : LSs){
+                (*lsp_it)->line = (*lsp_it)->line.Moving_Median_Filter_Two_Sided_Gaussian_Weighting(sigma);
+            }
+
+        // moving-median-filter-two-sided-triangular-weighting
+        }else if(rg.matches(pf.name, method_mmftw)){
+            if(pf.parameters.size() != 1UL){
+                throw std::invalid_argument("Incorrect number of arguments");
+            }
+            const auto n = pf.parameters.at(0).number.value();
+            const auto N = static_cast<int64_t>(n);
+
+            for(auto & lsp_it : LSs){
+                (*lsp_it)->line = (*lsp_it)->line.Moving_Median_Filter_Two_Sided_Triangular_Weighting(N);
+            }
+
+        // moving-variance-two-sided
+        }else if(rg.matches(pf.name, method_mv)){
+            if(pf.parameters.size() != 1UL){
+                throw std::invalid_argument("Incorrect number of arguments");
+            }
+            const auto n = pf.parameters.at(0).number.value();
+            const auto N = static_cast<int64_t>(n);
+
+            for(auto & lsp_it : LSs){
+                (*lsp_it)->line = (*lsp_it)->line.Moving_Variance_Two_Sided(N);
+            }
+
+        // derivative-forward-finite-differences
+        }else if(rg.matches(pf.name, method_dffd)){
+            if(pf.parameters.size() != 0UL){
+                throw std::invalid_argument("Incorrect number of arguments");
+            }
+            for(auto & lsp_it : LSs){
+                (*lsp_it)->line = (*lsp_it)->line.Derivative_Forward_Finite_Differences();
+            }
+
+        // derivative-backward-finite-differences
+        }else if(rg.matches(pf.name, method_dbfd)){
+            if(pf.parameters.size() != 0UL){
+                throw std::invalid_argument("Incorrect number of arguments");
+            }
+            for(auto & lsp_it : LSs){
+                (*lsp_it)->line = (*lsp_it)->line.Derivative_Backward_Finite_Differences();
+            }
+
+        // derivative-centered-finite-differences
+        }else if(rg.matches(pf.name, method_dcfd)){
+            if(pf.parameters.size() != 0UL){
+                throw std::invalid_argument("Incorrect number of arguments");
+            }
+            for(auto & lsp_it : LSs){
+                (*lsp_it)->line = (*lsp_it)->line.Derivative_Centered_Finite_Differences();
+            }
+
+        // local-signed-curvature-three-sample
+        }else if(rg.matches(pf.name, method_lsc3s)){
+            if(pf.parameters.size() != 0UL){
+                throw std::invalid_argument("Incorrect number of arguments");
+            }
+            for(auto & lsp_it : LSs){
+                (*lsp_it)->line = (*lsp_it)->line.Local_Signed_Curvature_Three_Datum();
             }
 
 
