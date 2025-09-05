f554426ae07319a9d223c019c0ee1225934824a0
Hal Clark <gmail.com@hdeanclark>
2025-09-05 13:33:25 -0700

    scripts: URL checker: fallback on wget.

--- scripts/check_url_reachability.sh
+++ scripts/check_url_reachability.sh
@@ -21,6 +21,14 @@ any_failed=0
 for u in "${urls[@]}" ; do
     status_code="$( curl --output '/dev/null' --head --silent --retry 3 --retry-delay 15 --retry-connrefused --write-out "%{http_code}" "${u}" )"
 
+    # Attempt to use a fallback, which sometimes seems to work when the above fails.
+    if ! [[ "${status_code}" -eq 200 ||
+            "${status_code}" -eq 301 ||
+            "${status_code}" -eq 302 ]] ; then
+        printf '    Received code %s for %s -- retrying...\n' "${status_code}" "${u}"
+        status_code="$( wget -O '/dev/null' --tries 3 --timeout 15  --server-response --max-redirect=0 "${u}" 2>&1 | grep 'HTTP/' | awk '{print $2}' )"
+    fi
+
     printf '    Received code %s for %s\n' "${status_code}" "${u}"
 
     if ! [[ "${status_code}" -eq 200 ||
